06f50a8e060d6504ea3f2e085881b7f0a3f0e31b
Shawn Allen <shawn.allen@gsa.gov>
2015-06-26 11:32:41 -0700

    add basic school detail page

---
+++ js/school.js
@@ -0,0 +1,45 @@
+---
+# // hey there
+---
+(function(exports) {
+
+  var id = getSchoolId();
+
+  if (!id) {
+    return showError('No school ID provided');
+  }
+
+  var root = document.querySelector('#school');
+
+  var directives = {
+  };
+
+  API.getSchool(id, function(error, school) {
+    if (error) {
+      return showError(error);
+    }
+
+    console.log('got school:', school);
+    root.classList.remove('hidden');
+    tagalong(root, school, directives);
+  });
+
+  function getSchoolId() {
+    if (!location.search) return null;
+    var match = location.search.match(/^\?(\d+)(\b|-)/);
+    return match ? match[1] : null;
+  }
+
+  function redirect(uri) {
+    window.location = uri || '../search/';
+  }
+
+  function showError(message) {
+    var container = document.querySelector('#error');
+    container.classList.remove('hidden');
+    var target = container.querySelector('.error-message') || container;
+    target.textContent = message;
+    return target;
+  }
+
+})(this);
