b7b93219b1543401293ff2f0e122ca98e9b1dfaa
Shawn Allen <shawn.allen@gsa.gov>
2015-07-08 08:44:56 -0700

    add school info, map

--- js/search.js
+++ js/search.js
@@ -16,46 +16,7 @@
 
   expandAccordions(values);
 
-  /**
-   * This is our format generator. Its methods are format generators for
-   * specific types of values, and they take a key in the data object to
-   * format. For instance:
-   *
-   * @example
-   * var formatFoo = format.percent('foo');
-   * formatFoo({foo: 0.5}) -> '50%'
-   */
-  var format = (function() {
-    var formatter = function(fmt, _empty) {
-      if (typeof fmt === 'string') {
-        fmt = d3.format(fmt);
-      }
-      return function(key, empty) {
-        empty = empty || _empty;
-        if (typeof empty === 'string') {
-          empty = d3.functor(empty);
-        }
-        return function(d) {
-          var value = d[key];
-          return (!value && empty)
-            ? empty.call(d)
-            : fmt.call(d, +value, key);
-        };
-      };
-    };
-
-    return {
-      dollars: formatter('$,d', '$0'),
-      percent: formatter('%.0f', '--'),
-      number: formatter(',d', '0'),
-      plural: function(key, singular, plural) {
-        if (!plural) plural = singular + 's';
-        return function(d) {
-          return d[key] == 1 ? singular : plural;
-        };
-      }
-    };
-  })();
+  var format = picc.format;
 
   resultsRoot.classList.add('js-loading');
   API.search(values, function(error, data) {
@@ -84,7 +45,7 @@
 
     var resultsList = resultsRoot.querySelector('.schools-list');
     tagalong(resultsList, data.results, {
-      size: format.number('size'),
+      size_pretty: format.number('size'),
       link: {
         '@href': function(d) {
           var name = d.name.replace(/\W+/g, '-');
