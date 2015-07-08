d52b80c5fc1057afd0268738d2312973c26f00d3
Shawn Allen <shawn.allen@gsa.gov>
2015-07-08 10:48:55 -0700

    add more useful value formatters

--- js/picc.js
+++ js/picc.js
@@ -30,16 +30,84 @@
       };
     };
 
+    var map = function(keys, fallback) {
+      return function(key) {
+        return keys[key] || fallback;
+      };
+    };
+
+    var range = function(ranges) {
+      var len = ranges.length;
+      var i;
+      return function(value) {
+        value = +value;
+        for (i = 0; i < len; i++) {
+          var range = ranges[i];
+          if (value >= range[0] && value < range[1]) {
+            return range[2];
+          }
+        }
+      };
+    };
+
     return {
+      // format.dollars('x')({x: 1000}) === '$1,000'
       dollars: formatter('$,d', '$0'),
+      // format.percent('y')({x: 1000}) === '$1,000'
       percent: formatter('%.0f', '--'),
       number: formatter(',d', '0'),
+
+      // format.plural('x', 'foo')({x: 1}) === 'foo'
+      // format.plural('x', 'foo')({x: 2}) === 'foos'
       plural: function(key, singular, plural) {
         if (!plural) plural = singular + 's';
         return function(d) {
           return d[key] == 1 ? singular : plural;
         };
-      }
+      },
+
+      // format.map('x', {1: 'a'})({x: 1}) === 'a'
+      map: function(key, values, empty) {
+        return formatter(map(values))
+          .call(this, key, empty);
+      },
+
+      // format.control('control')({control: 1}) === 'Public'
+      // format.control('control')({control: 2}) === 'Private non-profit'
+      control: formatter(map({
+        '1': 'Public',
+        '2': 'Private non-profit',
+        '3': 'Private for-profit'
+      }, 'unknown')),
+
+      // format.preddeg('deg')({deg: 2}) === '2-year'
+      // format.preddeg('deg')({deg: 3}) === '4-year'
+      preddeg: formatter(map({
+        '2': '2-year',
+        '3': '4-year'
+      }), ''),
+
+      sizeCategory: formatter(range([
+        [0, 2000, 'Small'],
+        [2000, 15000, 'Medium'],
+        [15000, Infinity, 'Large']
+      ])),
+
+      // format.locale('locale')({locale: 11}) === 'City: Large'
+      locale: formatter(map({
+        '11': 'City: Large',
+        '12': 'City: Midsize',
+        '13': 'City: Small',
+        '21': 'Suburb: Large',
+        '22': 'Suburb: Midsize',
+        '23': 'Suburb: Small',
+        '31': 'Town: Fringe',
+        '32': 'Town: Distant',
+        '33': 'Town: Remote',
+        '41': 'Rural: Fringe',
+        '42': 'Rural: Distant',
+        '43': 'Rural: Remote'
+      }, 'unknown'))
     };
   })();
 
