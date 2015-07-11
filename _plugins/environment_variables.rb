cb3b464447c88b953d44a9a6169ec0b1630e7c1c
Shawn Allen <shawn.allen@gsa.gov>
2015-07-10 17:01:38 -0700

    import API_ env vars into site.config

---
+++ _plugins/environment_variables.rb
@@ -0,0 +1,21 @@
+# Plugin to add environment variables to the `site` object in Liquid templates
+
+module Jekyll
+
+  class EnvironmentVariablesGenerator < Generator
+
+    def generate(site)
+      if ENV['API_BASE_URL']
+        site.config['api']['baseurl'] = ENV['API_BASE_URL']
+      end
+      if ENV['API_KEY']
+        site.config['api']['key'] = ENV['API_KEY']
+      end
+      puts "API: ------------------"
+      puts site.config['api']
+      puts "------------------"
+    end
+
+  end
+
+end
