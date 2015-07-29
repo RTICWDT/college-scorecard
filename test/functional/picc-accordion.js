ad17515642faed9b5289c162ef3321fe446e049f
Shawn Allen <shawn.allen@gsa.gov>
2015-07-29 13:24:38 -0700

    increase the timeout for Sauce Labs

--- test/functional/picc-accordion.js
+++ test/functional/picc-accordion.js
@@ -1,10 +1,11 @@
 var BASE_URL = 'http://localhost:4000/college-choice/';
+var DEFAULT_TIMEOUT = 10000;
 
 module.exports = {
   'accordions are invisible by default': function(client) {
     client
       .url(BASE_URL)
-      .waitForElementPresent('.picc-accordion', 1000)
+      .waitForElementPresent('.picc-accordion', DEFAULT_TIMEOUT)
       .assert.hidden('.picc-accordion [aria-hidden]');
   }
 };
