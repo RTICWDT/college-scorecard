c72904026bb6b5fdc31f377ad0de597ce9c6becb
Shawn Allen <shawn.allen@gsa.gov>
2015-07-29 22:03:36 -0700

    shuffle around search commands

--- test/pages/search.js
+++ test/pages/search.js
@@ -1,30 +1,29 @@
 var assert = require('assert');
 
-var searchCommands = {
-  submitSearch: function() {
-    return this.waitForElementPresent('@searchSubmit')
-      .click('@searchSubmit')
-      .waitForElementNotPresent('@searchSubmit');
-  },
-
-  assertResultsCount: function(count) {
-    var client = this;
-    var selector = '[data-bind="results_total"]';
-    return this.execute(function(selector, count) {
-      return document.querySelector(selector).textContent;
-    }, [selector, count], function(text) {
-      assert.equal(text.replace(/[^\d]+/g), String(count));
-    });
-  },
-
-};
-
 module.exports = {
   url: function() {
     return this.api.launchUrl + 'search/';
   },
 
-  commands: [searchCommands],
+  commands: [{
+
+    submitSearch: function() {
+      return this.waitForElementPresent('@searchSubmit')
+        .click('@searchSubmit')
+        .waitForElementNotPresent('@searchSubmit');
+    },
+
+    assertResultsCount: function(count) {
+      var client = this;
+      var selector = '[data-bind="results_total"]';
+      return this.execute(function(selector, count) {
+        return document.querySelector(selector).textContent;
+      }, [selector, count], function(text) {
+        assert.equal(text.replace(/[^\d]+/g), String(count));
+      });
+    }
+
+  }],
 
   elements: {
     searchForm: {selector: '#search-form'},
