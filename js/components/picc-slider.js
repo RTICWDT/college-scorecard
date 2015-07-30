bba905e9586aee57ce86ed5104946cad0608cdd9
Shawn Allen <shawn.allen@gsa.gov>
2015-07-30 11:48:49 -0700

    first stab at range slider

---
+++ js/components/picc-slider.js
@@ -0,0 +1,285 @@
+(function(exports) {
+
+  var events = {
+    click: function(e) {
+      // ignore right-clicks
+      if (e.button === 2) return false;
+
+      this.__dragging = getClosestHandle.call(this, e);
+      events.move.call(this, e);
+    },
+
+    engage: function(e) {
+      // ignore right-clicks
+      if (e.button === 2) {
+        e.preventDefault();
+        return false;
+      }
+
+      this.__dragging = getClosestHandle.call(this, e);
+      this.classList.add('__dragging');
+
+      window.addEventListener('mousemove', getListener('move', this));
+      window.addEventListener('touchmove', getListener('move', this));
+      window.addEventListener('mouseup', getListener('release', this));
+      window.addEventListener('touchend', getListener('release', this));
+    },
+
+    move: function(e) {
+      if (!this.__dragging) return;
+
+      var handle = this.__dragging;
+      var x = getMouseX.call(this, e);
+
+      var property;
+      var fudge = 10;
+      if (handle === this.__left) {
+        property = 'lower';
+        var limit = this.__right.getBoundingClientRect().left - fudge;
+        if (x >= limit) return;
+      } else {
+        property = 'upper';
+        var limit = this.__left.getBoundingClientRect().right + fudge;
+        if (x <= limit) return;
+      }
+
+      var width = this.getBoundingClientRect().width;
+      x = Math.max(0, Math.min(x, width));
+
+      // console.log('click: ', e.clientX, ', ', rect.left, ' -> ', x);
+      var value = this.min + (x / width) * (this.max - this.min);
+      if (this.step) value = roundTo(value, this.step);
+      this[property] = value;
+      this.update();
+      this.setAttribute('aria-valuenow', value);
+      e.preventDefault();
+      return false;
+    },
+
+    release: function(e) {
+      this.__dragging = false;
+      this.classList.remove('__dragging');
+      this.removeAttribute('aria-valuenow');
+      window.removeEventListener('mousemove', getListener('move', this));
+      window.removeEventListener('touchmove', getListener('move', this));
+      window.removeEventListener('mouseup', getListener('release', this));
+      window.removeEventListener('touchend', getListener('release', this));
+      e.preventDefault();
+      return false;
+    },
+
+    keypress: function(e) {
+      // console.log('keypress:', e);
+      switch (e.keyCode) {
+        case 37: // left
+          nudge.call(this, -1);
+          break;
+        case 39: // right
+          nudge.call(this, +1);
+          break;
+      }
+    },
+
+    focus: function(e) {
+      window.addEventListener('keyup', getListener('keypress', this));
+      this.addEventListener('blur', events.blur);
+    },
+
+    blur: function(e) {
+      window.removeEventListener('keyup', getListener('keypress', this));
+      this.removeEventListener('blur', events.blur);
+    }
+
+  };
+
+
+  exports.PICCSlider = registerElement('picc-slider', {
+    createdCallback: function() {
+      this.__range = createRange.call(this);
+      this.__left = createHandle.call(this, 'min');
+      this.__right = createHandle.call(this, 'max');
+
+      this.setAttribute('aria-role', 'slider');
+      this.min = getAttr.call(this, 'min', 0);
+      this.max = getAttr.call(this, 'max', 100);
+      this.lower = getAttr.call(this, 'lower', 0);
+      this.upper = getAttr.call(this, 'upper', 100);
+      this.step = getAttr.call(this, 'step', 1);
+    },
+
+    attachedCallback: function() {
+      this.addEventListener('click', events.click);
+      this.addEventListener('mousedown', events.engage);
+      this.addEventListener('touchstart', events.engage);
+      this.addEventListener('focus', events.focus, true);
+    },
+
+    detachedCallback: function() {
+      // console.log('picc-slider detached');
+      this.removeEventListener('click', events.click);
+      this.removeEventListener('mousedown', events.enagage);
+      this.removeEventListener('touchstart', events.enagage);
+      this.removeEventListener('focus', events.focus, true);
+    },
+
+    attributeChangedCallback: function(attr, prev, value) {
+      switch (attr) {
+        case 'min':
+        case 'max':
+        case 'value':
+        case 'step':
+          // console.log('picc-slider attr: ', attr, ' = ', value);
+          this[attr] = value;
+          this.update();
+          break;
+      }
+    },
+
+    update: function() {
+      var min = this.min;
+      var max = this.max;
+      var lower = this.lower;
+      var upper = this.upper;
+
+      var x = function(value) {
+        return 100 * (value - min) / (max - min);
+      };
+
+      var left = percent(x(lower));
+      var right = percent(x(upper));
+
+      this.__left.style.setProperty('left', left);
+      this.__right.style.setProperty('left', right);
+
+      this.__range.style.setProperty('left', left);
+      this.__range.style.setProperty('right', percent(x(max - upper)));
+
+      this.setAttribute('aria-valuemin', lower);
+      this.setAttribute('aria-valuemax', upper);
+
+      this.dispatchEvent(new CustomEvent('change', {
+        min: lower,
+        max: upper
+      }));
+    },
+
+    // clamp to min and max, round if snap === true
+    value: property('value', function(v) {
+      v = clamp(+v, this.min, this.max);
+      if (this.step) v = roundTo(v, this.step);
+      return v;
+    }),
+
+    // parse min and max as numbers
+    min: property('min', Number),
+    max: property('max', Number),
+    lower: property('lower', Number),
+    upper: property('upper', Number),
+
+    // parse snap as a boolean
+    step: property('step', Number)
+  });
+
+  // so that event listeners
+  function getListener(type, obj) {
+    var key = '__' + type;
+    return obj[key] || (obj[key] = events[type].bind(obj));
+  }
+
+  function registerElement(name, proto, parent) {
+    if (!parent) parent = HTMLElement;
+    for (var key in proto) {
+      if (typeof proto[key] === 'function') {
+        proto[key] = {value: proto[key]};
+        if (key.indexOf('__') === 0) {
+          proto[key].enumerable = false;
+        }
+      }
+    }
+    return document.registerElement(name, {
+      prototype: Object.create(
+        parent.prototype,
+        proto
+      )
+    });
+  }
+
+  function getAttr(name, fallback) {
+    return this.hasAttribute(name)
+      ? this.getAttribute(name)
+      : fallback;
+  }
+
+  function property(name, parse) {
+    var key = '__' + name;
+    return {
+      get: function() {
+        return this[key];
+      },
+      set: function(value) {
+        if (parse) value = parse.call(this, value, name);
+        if (value !== this[key]) {
+          this[key] = value;
+          this.update();
+        }
+      }
+    };
+  }
+
+  function createHandle(which) {
+    var div = document.createElement('div');
+    div.className = 'handle handle_' + which;
+    div.setAttribute('tabindex', 0);
+    return this.appendChild(div);
+  }
+
+  function createRange() {
+    var div = document.createElement('div');
+    div.className = 'range';
+    return this.appendChild(div);
+  }
+
+  function getClosestHandle(e) {
+    switch (e.target) {
+      case this.__left: return this.__left;
+      case this.__right: return this.__right;
+    }
+
+    var x = getMouseX.call(this, e);
+    var left = this.__left.getBoundingClientRect().right;
+    var right = this.__right.getBoundingClientRect().left;
+    var dl = Math.abs(x - left);
+    var dr = Math.abs(x - right);
+    return dl < dr ? this.__left : this.__right;
+  }
+
+  function getMouseX(e) {
+    var rect = this.getBoundingClientRect();
+    var width = rect.width;
+    return e.clientX - rect.left;
+  }
+
+  function nudge(multiplier) {
+    var handle = this.__dragging;
+    if (!handle) return;
+
+    var property = handle === this.__left ? 'lower' : 'upper';
+    var step = this.step || .1;
+    this[property] += step * multiplier;
+  }
+
+  function clamp(x, min, max) {
+    if (x < min) return min;
+    else if (x > max) return max;
+    return x;
+  }
+
+  function roundTo(value, step) {
+    return Math.round(value / step) * step;
+  }
+
+  function percent(value) {
+    return value.toFixed(2) + '%';
+  }
+
+})(this);
