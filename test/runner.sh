767a29745a2028a11ad0cc619e251ffbf4d7e52b
Shawn Allen <shawn.allen@gsa.gov>
2015-07-30 00:31:53 -0700

    isolate multi-browser testing script

--- test/runner.sh
+++ test/runner.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-echo "testing environments: $@ ..."
 
 # start the jekyll server and remember its pid
 jekyll serve &
@@ -8,15 +7,7 @@ PID=$!
 # this should be enough time for jekyll to spin up
 sleep 3
 
-envs=$@
-if [ -z "${envs}" ]; then
-    envs=default
-fi
-
-for env in ${envs}; do
-    echo "testing ${env}..."
-    nightwatch --config ./test/nightwatch.js -e ${env}
-done
+./test/browsers.sh $@
 
 # kill the background jekyll server
-kill -9 ${PID}
+kill -9 ${PID} || echo "(Jekyll died before we could kill it.)"
