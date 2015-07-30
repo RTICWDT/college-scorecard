12c31f47dd477d1d579896ec755fa8860cfcb2f9
Shawn Allen <shawn.allen@gsa.gov>
2015-07-29 23:43:05 -0700

    turn this into a multi-browser thing

--- test/travis.sh
+++ test/travis.sh
@@ -1,6 +1,22 @@
 #!/bin/sh
+
+# start the jekyll server and remember its pid
 jekyll serve &
 PID=$!
-sleep 3 # this should be enough time for jekyll to spin up
-npm run test${1}
+
+# this should be enough time for jekyll to spin up
+sleep 3
+
+if [ "$#" -ne 1 ]; then
+    browsers=ie9
+else
+    browsers="$@"
+fi
+
+for browser in ${browsers}; do
+    echo "testing ${browser}..."
+    nightwatch --config ./test/nightwatch.js -e ${browser}
+done
+
+# kill the background jekyll server
 kill -9 ${PID}
