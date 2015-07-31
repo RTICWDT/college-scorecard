77be2eb98973060b2e6ea4139951c3a618fc2651
Shawn Allen <shawn.allen@gsa.gov>
2015-07-31 15:57:37 -0700

    refactor for dynamic updating

--- js/search.js
+++ js/search.js
@@ -1,74 +1,75 @@
 (function(exports) {
 
   var resultsRoot = document.querySelector('.search-results');
-  var query = location.search;
-  if (!query) {
-    resultsRoot.classList.add('hidden');
-    return;
-  }
 
-  // TODO: include formdb, querystring, and tagalong
   var form = new formdb.Form('#search-form');
-  var values = querystring.parse(query.substr(1));
-  removeEmptyValues(values);
-  console.debug('[search] form values:', values);
-  form.setData(values);
+
+  var query = querystring.parse(location.search.substr(1));
+  form.setData(query);
+
+  // the current outbound request
+  var req;
+
+  onChange(form.getData());
+
+  form.on('change', picc.debounce(onChange, 200));
+
+  form.on('submit', function(data, e) {
+    onChange(data);
+    e.preventDefault();
+    return false;
+  });
 
   picc.ui.expandAccordions(function() {
     var inputs = this.querySelectorAll('[name]');
     return [].some.call(inputs, function(input) {
-      return values[input.name];
+      return query[input.name];
     });
   });
 
-  var format = picc.format;
+  function onChange(params) {
+    var qs = querystring.stringify(params);
+    history.pushState(params, 'search', '?' + qs);
 
-  resultsRoot.classList.add('js-loading');
-  picc.API.search(values, function(error, data) {
-    resultsRoot.classList.remove('hidden');
-    resultsRoot.classList.remove('js-loading');
+    if (req) req.cancel();
 
-    if (error) {
-      return showError(error);
-    }
+    resultsRoot.classList.add('js-loading');
+    picc.API.search(params, function(error, data) {
+      resultsRoot.classList.remove('hidden');
+      resultsRoot.classList.remove('js-loading');
 
-    console.log('loaded schools:', data);
-    resultsRoot.classList.add('js-loaded');
+      if (error) {
+        return showError(error);
+      }
 
-    console.time('[render]');
+      var format = picc.format;
 
-    console.time('[render] template');
-    // render the basic DOM template for each school
-    tagalong(resultsRoot, data, {
-      results_word: format.plural('total', 'Result'),
-      results_total: format.number('total', '0')
+      console.log('loaded schools:', data);
+      resultsRoot.classList.add('js-loaded');
+
+      console.time('[render]');
+
+      console.time('[render] template');
+      // render the basic DOM template for each school
+      tagalong(resultsRoot, data, {
+        results_word: format.plural('total', 'Result'),
+        results_total: format.number('total', '0')
+      });
+
+      var resultsList = resultsRoot.querySelector('.schools-list');
+      tagalong(resultsList, data.results, picc.school.directives);
+
+      console.timeEnd('[render] template');
+
+      console.timeEnd('[render]');
     });
-
-    var resultsList = resultsRoot.querySelector('.schools-list');
-    tagalong(resultsList, data.results, picc.school.directives);
-
-    console.timeEnd('[render] template');
-
-    console.timeEnd('[render]');
-  });
+  }
 
   function showError(error) {
     console.error('error:', error);
     resultsRoot.classList.add('js-error');
     var message = resultsRoot.querySelector('.error-message');
     message.textContent = String(error.responseText || 'There was an unexpected API error.');
-  }
-
-  function removeEmptyValues(obj) {
-    var empty = function(v) {
-      return v === null || v === '';
-    };
-    for (var key in obj) {
-      if (empty(obj[key])) {
-        delete obj[key];
-      }
-    }
-    return obj;
   }
 
 })(this);
