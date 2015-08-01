1e41c8511d3dd0ad231a0a35d49ffd8beb96840f
Shawn Allen <shawn.allen@gsa.gov>
2015-08-01 09:31:51 -0700

    better tests for aria-accordion

--- test/functional/accordion.js
+++ test/functional/accordion.js
@@ -1,14 +1,45 @@
 var sauce = require('../sauce');
 
 module.exports = {
-  'accordions are invisible by default': function(client) {
-    // there's guaranteed to be an accordion on the
-    // styleguide page
+
+  before: function(client) {
+    client.windowSize('current', 1024, 768);
+  },
+
+  'collapsed accordions have invisible content by default': function(client) {
     client.page.styleguide()
       .navigate()
-      .waitForElementPresent('.picc-accordion')
-      .assert.hidden('.picc-accordion [aria-hidden]');
+      .waitForElementPresent('@collapsedAccordion')
+      .assert.hidden('@collapsedAccordionContent');
   },
+
+  'expanded accordions have visible content by default': function(client) {
+    client.page.styleguide()
+      .navigate()
+      .waitForElementPresent('@expandedAccordion')
+      .assert.visible('@expandedAccordionContent');
+  },
+
+  /*
+  // FIXME: these don't work because Nightwatch/Selenium
+  // can't find the trigger selectors???
+  'expanded accordions should close when clicked': function(client) {
+    client.page.styleguide()
+      .navigate()
+      .waitForElementPresent('@expandedAccordionTrigger')
+      .waitForElementPresent('@expandedAccordionContent')
+      .click('@expandedAccordionTrigger')
+      .assert.hidden('@expandedAccordionContent');
+  },
+
+  'collapsed accordions should open when clicked': function(client) {
+    client.page.styleguide()
+      .navigate()
+      .waitForElementPresent('@collapsedAccordion')
+      .click('@collapsedAccordionTrigger')
+      .assert.visible('@collapsedAccordionContent');
+  },
+  */
 
   after: function(client) {
     client.end();
--- test/pages/styleguide.js
+++ test/pages/styleguide.js
@@ -4,6 +4,12 @@ module.exports = {
   },
 
   elements: {
+    expandedAccordion:          'aria-accordion[aria-expanded="true"]',
+    expandedAccordionContent:   'aria-accordion[aria-expanded="true"] div',
+    expandedAccordionTrigger:   'aria-accordion[aria-expanded="true"] [aria-controls]',
+    collapsedAccordion:         'aria-accordion[aria-expanded="false"]',
+    collapsedAccordionContent:  'aria-accordion[aria-expanded="false"] div',
+    collapsedAccordionTrigger:  'aria-accordion[aria-expanded="false"] [aria-controls]',
   },
 
   sections: {
