a66141d675316f1be74db96ff224955165e55d66
Shawn Allen <shawn.allen@gsa.gov>
2015-08-03 14:24:41 -0700

    add document ready command

--- test/nightwatch.js
+++ test/nightwatch.js
@@ -91,6 +91,7 @@ module.exports = {
   parallel_process_delay: 10,
 
   custom_assertions_path: '',
+  custom_commands_path: './test/commands',
   globals_path: './test/globals.js',
 
   selenium: {
