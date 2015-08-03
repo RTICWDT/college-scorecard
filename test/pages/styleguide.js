e8b1776d6e175fddebdc7f06d26cf75e082e6f6f
Shawn Allen <shawn.allen@gsa.gov>
2015-08-03 11:53:17 -0700

    use class hooks for accordion parts

--- test/pages/styleguide.js
+++ test/pages/styleguide.js
@@ -5,11 +5,11 @@ module.exports = {
 
   elements: {
     expandedAccordion:          'aria-accordion[aria-expanded="true"]',
-    expandedAccordionContent:   'aria-accordion[aria-expanded="true"] div',
-    expandedAccordionTrigger:   'aria-accordion[aria-expanded="true"] [aria-controls]',
+    expandedAccordionContent:   'aria-accordion[aria-expanded="true"] .content',
+    expandedAccordionTrigger:   'aria-accordion[aria-expanded="true"] .trigger',
     collapsedAccordion:         'aria-accordion[aria-expanded="false"]',
-    collapsedAccordionContent:  'aria-accordion[aria-expanded="false"] div',
-    collapsedAccordionTrigger:  'aria-accordion[aria-expanded="false"] [aria-controls]',
+    collapsedAccordionContent:  'aria-accordion[aria-expanded="false"] .content',
+    collapsedAccordionTrigger:  'aria-accordion[aria-expanded="false"] .trigger',
   },
 
   sections: {
