f009116d98b2b3de64db7fb61b52afbec259e735
Shawn Allen <shawn.allen@gsa.gov>
2015-08-03 14:25:34 -0700

    add CustomEvent polyfill for PhantomJS (and oldIE)

--- js/components/aria-accordion.js
+++ js/components/aria-accordion.js
@@ -4,6 +4,23 @@
   var HIDDEN = 'aria-hidden';
   var TOGGLE_EVENTS = ['click', 'touchstart'];
 
+  // because PhantomJS and oldIE don't implement CustomEvent
+  if (!window.CustomEvent) {
+    var CustomEvent = function(event, params) {
+      var evt;
+      params = params || {
+        bubbles: false,
+        cancelable: false,
+        detail: undefined
+      };
+      evt = document.createEvent("CustomEvent");
+      evt.initCustomEvent(event, params.bubbles, params.cancelable, params.detail);
+      return evt;
+    };
+    CustomEvent.prototype = window.Event.prototype;
+    window.CustomEvent = CustomEvent;
+  }
+
   exports.ARIAAccordion = document.registerElement('aria-accordion', {
     prototype: Object.create(
       HTMLElement.prototype,
