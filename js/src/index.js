97026d621a027e4272ce1a8dddd0d585e6984109
Shawn Allen <shawn.allen@gsa.gov>
2015-09-01 13:55:13 -0700

    add page script sources

---
+++ js/src/index.js
@@ -0,0 +1,19 @@
+var formdb = require('formdb');
+
+module.exports = function index() {
+
+  var form = new formdb.Form('#search-form');
+  picc.form.minifyQueryString(form);
+
+  picc.ready(function() {
+    if (location.hash.length > 1) {
+      var id = location.hash.substr(1);
+      picc.ui.expandAccordions(function() {
+        return this.id && this.id === id;
+      });
+      location.hash = '';
+      location.hash = '#' + id;
+    }
+  });
+
+};
