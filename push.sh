2992fce5f99de1a4eba5c40e4f7b4aa5766d9b5e
Sarah Allen <sarah.allen@gsa.gov>
2015-08-31 23:07:20 -0400

    production space renamed: prod => production

--- push.sh
+++ push.sh
@@ -2,6 +2,6 @@
 
 : ${API_KEY?"The API_KEY environment variable needs to be set."}
 : ${API_BASE_URL?"The API_BASE_URL environment variable needs to be set."}
-cf target -o ed -s prod
+cf target -o ed -s production
 jekyll build --config _config.yml,_config_cf.yml
 cf push college-choice
