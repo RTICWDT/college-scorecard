a7666de1a22f9d32ac03a07a8ffaa1bdde072e74
Shawn Allen <shawn.allen@gsa.gov>
2015-09-01 17:07:36 -0700

    fix minifyify args source map path

--- js/src/browserify-args.sh
+++ js/src/browserify-args.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-echo '-v -d -s picc -p [ minifyify --map /js/picc.js.map --output js/picc.js.map ] -o js/picc.js js/src/picc.js'
+echo '-v -d -s picc -p [ minifyify --map picc.js.map --output js/picc.js.map ] -o js/picc.js js/src/picc.js'
