07ac6cedc28caab5a1e924f7af2b49e418a527e3
Shawn Allen <shawn.allen@gsa.gov>
2015-09-10 13:05:07 -0700

    exit 1 from test/runner.sh

--- test/runner.sh
+++ test/runner.sh
@@ -7,7 +7,7 @@ PID=$!
 # this should be enough time for jekyll to spin up
 sleep 3
 
-./test/browsers.sh $@
+./test/browsers.sh $@ || exit 1
 
 # kill the background jekyll server
 kill -9 ${PID} || echo "(Jekyll died before we could kill it.)"
