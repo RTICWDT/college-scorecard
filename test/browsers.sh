97b70735b58c8a0673a4db378f9aa4e8cb99f43d
Shawn Allen <shawn.allen@gsa.gov>
2015-09-10 13:38:37 -0700

    exit properly when a browser test fails

--- test/browsers.sh
+++ test/browsers.sh
@@ -8,5 +8,9 @@ fi
 
 for env in ${envs}; do
     echo "* testing ${env}..."
-    ./node_modules/.bin/nightwatch --config ./test/nightwatch.js -e ${env} || exit 1
+    ./node_modules/.bin/nightwatch --config ./test/nightwatch.js -e ${env}
+    if [ $? != 0 ]; then
+        echo "*** failed: ${env}!"
+        exit 1
+    fi
 done
