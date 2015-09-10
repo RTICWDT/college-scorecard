561813059e162b23928d724938200f8156acba6f
Shawn Allen <shawn.allen@gsa.gov>
2015-09-10 11:33:14 -0700

    exit from browsers.sh with 1 if a test fails

--- test/browsers.sh
+++ test/browsers.sh
@@ -8,5 +8,5 @@ fi
 
 for env in ${envs}; do
     echo "* testing ${env}..."
-    ./node_modules/.bin/nightwatch --config ./test/nightwatch.js -e ${env}
+    ./node_modules/.bin/nightwatch --config ./test/nightwatch.js -e ${env} || exit 1
 done
