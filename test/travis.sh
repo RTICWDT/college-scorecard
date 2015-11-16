172405b4bea1019d9e13482a45f4e275fb57a149
Yoz (Jeremy) Grahame <jeremy.grahame@gsa.gov>
2015-11-16 15:46:28 -0800

    Replace Travis with Circle, kill stale tests
    Remove .travis.yml (+33 squashed commits)
    Squashed commits:
    [0a2f907] Update testing README, use Chrome by default
    [d07e731] Fix tunnel-identifier actually properly really
    [e8dfd5a] Remove baseurl from Jekyll config
    [6652e44] Use a bigger string for job/tunnel ID
    [2496d49] Let's try tunnel IDs again
    [9130ab2] Update webdriverio and other packages
    [f638d89] Let's try without a tunnel identifier
    [c394c9b] Output tunnel ID from test script
    [83ce8fd] Fix SC tunnel identifier name
    [a7e7d17] Does $CIRCLE_BUILD_NUM actually work?
    [5c44214] Add test for Sauce tunnel readiness
    [1656b85] Move the server-start test to the right place
    [a3ef52d] Actually test for HTTP server start, really
    
    Proper test script exit code
    [d39ff7d] Test for HTTP server start instead of constant wait
    [5629d18] Testing CircleCI default npm install
    [7994249] ... and one more length fix
    [61091a5] Fix line lengths to keep Hound happy
    [45869c6] Open Sauce tunnel using CIRCLE_BUILD_NUM
    [8565380] Rename travis filenames to ci
    [6f7ce44] Remove old Nightwatch tests
    [611bb1e] ... and remember to re-download the SC tools
    [837cd85] sauceconnect-runner build does nasty things, dunno why, REVERT
    [529fc68] Update sauceconnect-runner (thanks, Shawn)
    [e4e7883] Use sauceconnect-runner
    [f36d468] Bundle install with CircleCI cache check
    [e0ec5af] Sleep before starting tests (for Sauce Connect setup)
    [c115f33] Jekyll: exclude "vendor/"
    [505deef] Use bin paths instead of global module install
    [fc85fdc] npm global install, bundle install restored
    [1522472] Node version and dev dependency install
    [72bd61b] Update npm modules
    [affb38f] Install and run Sauce Connect on Circle
    [b1c8de3] initial circle.yml

--- test/travis.sh
+++
@@ -1,5 +0,0 @@
-jekyll build
-http-server -p 4000 _site &
-PID=$!
-wdio test/wdio.travis.js
-kill $PID
--- test/wdio.travis.js
+++
@@ -1,9 +0,0 @@
-var config = require('./wdio.sauce').config;
-
-var job = process.env.TRAVIS_JOB_NUMBER;
-if (job) {
-  config['tunnel-identifier'] = job;
-  config.build = job;
-}
-
-exports.config = config;
