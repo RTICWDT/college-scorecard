ad5023a85b121e56c6a5a339ee3510d21dc20f67
David Best <david.best@gsa.gov>
2015-12-02 18:27:58 -0500

    Implementing the search test plan.
    This implements the search test plan. Places where the test results diverge from the plan are currently marked with `// CLARIFICATION` or `// CLAR ` comments.

--- test/wdio.conf.js
+++ test/wdio.conf.js
@@ -92,7 +92,8 @@ exports.config = {
     // Options to be passed to Mocha.
     // See the full list at http://mochajs.org/
     mochaOpts: {
-        ui: 'bdd'
+        ui: 'bdd',
+        timeout: 200000
     },
     
     //
