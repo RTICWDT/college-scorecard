c34f7da4df8d1373cdf69525b5cc06e89a1cb881
Shawn Allen <shawn.allen@gsa.gov>
2015-12-07 17:26:12 -0800

    xunit output; print the cwd

--- test/wdio.ci.js
+++ test/wdio.ci.js
@@ -2,8 +2,8 @@ var config = require('./wdio.sauce').config;
 
 var job = process.env.CIRCLE_BUILD_NUM;
 if (job) {
-  var jobID = "18f-cs-"+job;
-  console.log("Looking for tunnel ID: "+jobID);
+  var jobID = '18f-cs-' + job;
+  console.log('Looking for tunnel ID:', jobID);
   config.capabilities.forEach(function(caps) {
     caps['tunnel-identifier'] = jobID;
   });
