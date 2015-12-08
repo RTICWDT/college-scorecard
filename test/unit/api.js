f882a25120bb7488a6b2b39cb12ee6294593b1ed
Shawn Allen <shawn.allen@gsa.gov>
2015-12-07 16:08:53 -0800

    remove picc.API tests from unit test suite

--- test/unit/api.js
+++ test/unit/api.js
@@ -5,26 +5,12 @@ var picc = require('../../js/src/picc');
 var assert = require('assert');
 var extend = require('extend');
 
-const DEFAULT_API_URL = 'https://api.data.gov/ed/collegescorecard/v1/';
-
 // expected default parameters
 const EXPECTED_DEFAULTS = {
   '2013.student.size__range': '0..',
   'school.degrees_awarded.predominant__range': '2..3',
   'school.operating': '1'
 };
-
-var xhr = require('xmlhttprequest');
-// make XMLHttpRequest available to d3
-global.XMLHttpRequest = xhr.XMLHttpRequest;
-
-// use the API_BASE_URL environment variable,
-// or production API by default
-picc.API.url = process.env.API_BASE_URL || DEFAULT_API_URL;
-
-// XXX you'll need to set the API_KEY environment varible
-// for this to work
-picc.API.key = process.env.API_KEY;
 
 /**
  * Merge defaults into a set of parameters and optionally exclude one
@@ -44,21 +30,6 @@ var fromDefaults = function(params, except) {
   }
   return params;
 };
-
-describe('picc.API', function() {
-  // 10-second timeout on API requests
-  this.timeout(10000);
-
-  it('can request stuff', function(done) {
-    var params = {'school.name': 'appalachian'};
-    picc.API.get('schools/', params, function(error, data) {
-      assert.ok(!error);
-      assert.ok(data.results.length > 1,
-                'results: ' + data.results.length);
-      done();
-    });
-  });
-});
 
 /**
  * picc.form.prepareParams() is the function that we pass the search
