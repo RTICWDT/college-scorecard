d1742c5c71a03dcbb0eca369b6b462f1bf685b7f
Shawn Allen <shawn.allen@gsa.gov>
2015-12-07 16:51:44 -0800

    use sinon to stub d3.json calls

--- test/unit/api.js
+++ test/unit/api.js
@@ -1,65 +1,23 @@
 /* jshint esnext: true */
 /* globals require, process, global */
-
 var picc = require('../../js/src/picc');
 var assert = require('assert');
-var extend = require('extend');
+var sinon = require('sinon');
+var d3 = require('d3');
 
-// expected default parameters
-const EXPECTED_DEFAULTS = {
-  '2013.student.size__range': '0..',
-  'school.degrees_awarded.predominant__range': '2..3',
-  'school.operating': '1'
-};
+describe('picc.API', function() {
+  picc.API.url = '';
+  picc.API.key = '';
 
-/**
- * Merge defaults into a set of parameters and optionally exclude one
- * or more named parameters from the result.
- *
- * @param Object  params    an object literal of API parameters
- * @param Array   [except]  an optional list of keys to exclude from
- *                          the resulting object
- * @return Object
- */
-var fromDefaults = function(params, except) {
-  params = extend({}, EXPECTED_DEFAULTS, params);
-  if (except) {
-    except.forEach(function(key) {
-      delete params[key];
-    });
-  }
-  return params;
-};
+  describe('get()', function() {
 
-/**
- * picc.form.prepareParams() is the function that we pass the search
- * form input values to to generate the right API parameters.
- */
-describe('picc.form.prepareParams()', function() {
-  var prep = picc.form.prepareParams;
-
-  it('defaults: `size > 0`, `preddeg = 2..3`, `curroper = 1`', function() {
-    assert.deepEqual(prep({}), EXPECTED_DEFAULTS);
-  });
-
-  it('uses "school.name" instead of "name"', function() {
-    assert.deepEqual(prep({name: 'foo'}), fromDefaults({
-      'school.name': 'foo'
-    }));
-  });
-
-  describe('majors', function() {
-
-    it('can query associates degrees', function() {
-      assert.deepEqual(prep({
-        degree: 'a',
-        major: 'science_technology'
-      }), fromDefaults({
-        'school.degrees_awarded.predominant': '2',
-        '2013.academics.program.assoc.science_technology__range': '1..'
-      }, [
-        'school.degrees_awarded.predominant__range'
-      ]));
+    it('knows how to add the API key to requests', function() {
+      var spy = sinon.stub(d3, 'json');
+      picc.API.key = 'bar';
+      var cb = function() { };
+      picc.API.get('foo', cb);
+      assert.ok(spy.calledWith('foo?api_key=bar'));
+      d3.json.restore();
     });
 
   });
