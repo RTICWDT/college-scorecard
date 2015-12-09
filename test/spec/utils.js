4d73cbb2212d9579cfa41880a8644610b9b30a99
David Best <david.best@gsa.gov>
2015-12-09 15:06:17 -0500

    Increasing timeout for the tests.

--- test/spec/utils.js
+++ test/spec/utils.js
@@ -2,7 +2,7 @@
 /* global exports, browser */
 exports.getSearchCount = function*() {
   var value = yield browser
-    .pause(500)
+    .pause(800)
     .waitForText(
       '.results-main-alert .u-group_inline-left h1 span:first-child', 10000)
     .getText('.results-main-alert .u-group_inline-left h1 span:first-child');
