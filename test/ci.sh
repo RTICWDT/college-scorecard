8b0179dedf4584c8557c9230e579c671534e4425
Shawn Allen <shawn.allen@gsa.gov>
2016-01-14 16:14:00 -0800

    test with different API_BASE_URL depending on branch

--- test/ci.sh
+++ test/ci.sh
@@ -1,3 +1,29 @@
+# test against different APIs based on the branch being built
+if [ $CIRCLE_BRANCH ]; then
+    if [ $branch == 'master' ]; then
+
+        echo "[using the default (production) API]"
+
+    elif [ $branch == 'staging' ]; then
+
+        echo "[using the staging API]"
+        API_BASE_URL=https://api.data.gov/TEST/ed/staging/v1/
+
+    else
+
+        # the assumption here is that all dev branches should
+        # run against the dev API
+        echo "[using the dev API]"
+        API_BASE_URL=https://api.data.gov/TEST/ed/dev/v1/
+
+    fi
+fi
+
+if [ $API_BASE_URL ]; then
+    echo "building with API_BASE_URL=$API_BASE_URL ..."
+fi
+
+# build the site
 bundle exec jekyll build
 ./node_modules/.bin/http-server -p 4000 _site &
 wget --retry-connrefused --waitretry=1 -T 5 -t 30 http://localhost:4000 || exit 1
