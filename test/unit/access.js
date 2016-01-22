c2ff0a56f72ef36746b4a97096133eddd5660b09
Shawn Allen <shawn.allen@gsa.gov>
2016-01-22 10:36:33 -0800

    add unit tests for retention rate accessor

---
+++ test/unit/access.js
@@ -0,0 +1,42 @@
+var picc = require('../../js/src/picc');
+var assert = require('assert');
+
+describe('picc.access', function() {
+
+  describe('picc.access.retentionRate()', function() {
+
+    it('uses the four_year.full_time retention rate if it exists', function() {
+      var value = picc.access.retentionRate({
+        2013: {
+          student: {
+            retention_rate: {
+              four_year: {
+                full_time: 1.0
+              }
+            }
+          }
+        }
+      });
+      assert.equal(value, 1.0);
+    });
+
+    it('uses the lt_four_year.full_time retention rate if 4-year doesn\'t exist', function() {
+      var value = picc.access.retentionRate({
+        2013: {
+          student: {
+            retention_rate: {
+              four_year: {
+                full_time: null
+              },
+              lt_four_year: {
+                full_time: 0.5
+              }
+            }
+          }
+        }
+      });
+      assert.equal(value, 0.5);
+    });
+  });
+
+});
