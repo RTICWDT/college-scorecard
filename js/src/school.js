9694592fe801f9e155d54a1cf46630b0ecffb826
Shawn Allen <shawn.allen@gsa.gov>
2016-01-21 16:02:20 -0800

    fix line length

--- js/src/school.js
+++ js/src/school.js
@@ -208,8 +208,8 @@ module.exports = function school() {
         prefix: false
       })
       .addAttribution([
-        'Map tiles by <a href="http://stamen.com">Stamen Design</a>, ',
-        'under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>.',
+        'Map tiles by <a href="http://stamen.com">Stamen Design</a>, under ',
+        '<a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>.',
         '<br>Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, ',
         'under <a href="http://www.openstreetmap.org/copyright">ODbL</a>.'
       ].join(''))
