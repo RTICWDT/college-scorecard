77a414b2f433c3a7eb186afb4a84c3a75a5f70f7
Patrick Brown <brownpl@gmail.com>
2016-11-08 13:47:27 -0800

    Swapping out another instance of a green and updating flicky to include aria tags.

--- js/vendor/flickity.pkgd.js
+++ js/vendor/flickity.pkgd.js
@@ -3845,6 +3845,14 @@ PrevNextButton.prototype._create = function() {
   element.className += this.isPrevious ? ' previous' : ' next';
   // prevent button from submitting form http://stackoverflow.com/a/10836076/182183
   element.setAttribute( 'type', 'button' );
+  if(this.isPrevious)
+  {
+    element.setAttribute( 'aria-label', 'Previous');
+  }
+  else
+  {
+      element.setAttribute( 'aria-label', 'Next');
+  }
   Flickity.setUnselectable( element );
   // create arrow
   if ( supportsInlineSVG() ) {
