8a8f5a3352be323544e5918961aad9821ec5fd06
Miles Fink <miles@kynetiv.com>
2019-03-25 16:04:55 -0400

    Upgrade webcomponents (#1703)
    * upgrade document-register-element
    
    * create es5 style customElements base element and upgrade ARIAAccordion webcomponent to v1
    
    * add babelify transform and necessary fixes
    
    * migrate web components to native v1 and add fallback
    
    * require polyfill conditionally instead of inline script

--- js/src/components/multi-select.js
+++ js/src/components/multi-select.js
@@ -2,156 +2,150 @@
 
   var ITEM_CLASS = 'multi-select_item';
   var d3 = require('d3');
+  var HTMLParsedElement = require('./html-parsed-element');
 
-  exports.MultiSelect = document.registerElement('multi-select', {
-    prototype: Object.create(
-      HTMLElement.prototype,
-      {
-        createdCallback: {value: function() {
-        }},
+  exports.customElements.define(
+    'multi-select',
+    class extends HTMLParsedElement {
 
-        attachedCallback: {value: function() {
-          var select = this.querySelector('select');
-          if (!select) return console.error('no <select> found in <multi-select>!');
+      parsedCallback() {
+        var select = this.querySelector('select');
+        if (!select) return console.error('no <select> found in <multi-select>!');
 
-          // console.warn('<multi-select>');
+        // console.warn('<multi-select>');
 
-          // remove the multiple attribute, the clone it
-          select.removeAttribute('multiple');
-          this.__select = select.cloneNode(true);
-          removeEmptyOption(this.__select);
+        // remove the multiple attribute, the clone it
+        select.removeAttribute('multiple');
+        this.__select = select.cloneNode(true);
+        removeEmptyOption(this.__select);
 
-          // remove the name from the templated one
-          select.name = '';
+        // remove the name from the templated one
+        select.name = '';
 
-          // then set the multiple attribute
-          this.__select.setAttribute('multiple', 'multiple');
+        // then set the multiple attribute
+        this.__select.setAttribute('multiple', 'multiple');
 
-          this.createTemplate();
-          this.insertBefore(this.__select, this.firstChild);
+        this.createTemplate();
+        this.insertBefore(this.__select, this.firstChild);
 
-          this.value = select.value;
+        this.value = select.value;
 
-          // listen for change events, and hide it
-          this.__select.addEventListener('change', this.__onchange = onchange.bind(this));
-          this.__select.style.display = 'none';
-          this.__select.setAttribute('aria-hidden', 'true');
+        // listen for change events, and hide it
+        this.__select.addEventListener('change', this.__onchange = onchange.bind(this));
+        this.__select.style.display = 'none';
+        this.__select.setAttribute('aria-hidden', 'true');
 
-          this.__pollID = setInterval(pollSourceSelect.bind(this), 200);
-        }},
+        this.__pollID = setInterval(pollSourceSelect.bind(this), 200);
+      }
 
-        attributeChangedCallback: {value: function(attr, oldValue, newValue) {
-        }},
+      attributeChangedCallback(attr, oldValue, newValue) { }
 
-        detachedCallback: {value: function() {
-          this.__select.removeEventListener('change', this.__onchange);
-          clearTimeout(this.__pollID);
-        }},
+      disconnectedCallback() {
+        this.__select.removeEventListener('change', this.__onchange);
+        clearTimeout(this.__pollID);
+      }
 
-        update: {value: function() {
-          var root = d3.select(this);
-          var values = this.value;
-          // console.log('[update]');
+      update() {
+        var root = d3.select(this);
+        var values = this.value;
+        // console.log('[update]');
 
-          var set = (function(v, focus) {
-            // console.log('[set] values:', v);
-            this.value = v;
-            if (focus) {
-              this.querySelector('.' + ITEM_CLASS + ':last-child select').focus();
-            }
-          }).bind(this);
+        var set = (function(v, focus) {
+          // console.log('[set] values:', v);
+          this.value = v;
+          if (focus) {
+            this.querySelector('.' + ITEM_CLASS + ':last-child select').focus();
+          }
+        }).bind(this);
 
-          var items = root
-            .selectAll('.' + ITEM_CLASS)
-            .data(values);
+        var items = root
+          .selectAll('.' + ITEM_CLASS)
+          .data(values);
 
-          var template = this.__template;
-          items.exit().remove();
-          items.enter()
-            .append(function() {
-              return template.cloneNode(true);
-            })
-            .classed(ITEM_CLASS, true);
-
-          items.select('select')
-            .property('value', function(d) {
-              return nullish(d) ? '' : String(d);
-            })
-            .on('change', function(d, i) {
-              // console.log('[change %d]', i, this.value);
-              values[i] = this.value;
-              set(values);
-            });
-
-          /*
-          items.select('button')
-            .attr('disabled', 'disabled');
-          */
-
-          items.selectAll('button')
-            .on('click.cancel', function() {
-              d3.event.preventDefault();
-            });
-
-          items.each(function(value, i) {
-            var item = d3.select(this);
-            item.select('.button-remove')
-              .on('click', remove);
-            item.select('.button-add')
-              .on('click', add);
-            function remove() {
-              item.remove();
-              values.splice(i, 1);
-              // console.log('[remove %d]:', i, value);
-              // console.log('values:', values);
-              set(values);
-            }
+        var template = this.__template;
+        items.exit().remove();
+        items.enter()
+          .append(function() {
+            return template.cloneNode(true);
           })
+          .classed(ITEM_CLASS, true);
 
-          function add() {
-            values.push(null);
-            set(values, true);
+        items.select('select')
+          .property('value', function(d) {
+            return nullish(d) ? '' : String(d);
+          })
+          .on('change', function(d, i) {
+            // console.log('[change %d]', i, this.value);
+            values[i] = this.value;
+            set(values);
+          });
+
+        /*
+        items.select('button')
+          .attr('disabled', 'disabled');
+        */
+
+        items.selectAll('button')
+          .on('click.cancel', function() {
+            d3.event.preventDefault();
+          });
+
+        items.each(function(value, i) {
+          var item = d3.select(this);
+          item.select('.button-remove')
+            .on('click', remove);
+          item.select('.button-add')
+            .on('click', add);
+          function remove() {
+            item.remove();
+            values.splice(i, 1);
+            // console.log('[remove %d]:', i, value);
+            // console.log('values:', values);
+            set(values);
           }
-        }},
+        })
 
-        createTemplate: {value: function() {
-          if (!this.__template) {
-            var html = this.innerHTML;
-            var template = this.__template = document.createElement('div');
-            template.className = ITEM_CLASS;
-            template.innerHTML = html;
-            while (this.firstChild) {
-              this.removeChild(this.firstChild);
-            }
-            return template;
-          }
-          return this.__template;
-        }},
-
-        value: {
-          get: function() {
-            return Array.isArray(this.__value)
-              ? this.__value.slice()
-              : this.__value || [null];
-          },
-          set: function(value) {
-            // console.log('[set values]:', value);
-            if (compare(this.__value, value)) {
-              // console.warn('no change');
-              return;
-            }
-
-            this.__value = parseValues(value);
-
-            this.__updating = true;
-            this.update();
-            setValue(this.__select, this.__value);
-            this.__updating = false;
-          }
+        function add() {
+          values.push(null);
+          set(values, true);
         }
       }
-    )
-  });
+
+      createTemplate() {
+        if (!this.__template) {
+          var html = this.innerHTML;
+          var template = this.__template = document.createElement('div');
+          template.className = ITEM_CLASS;
+          template.innerHTML = html;
+          while (this.firstChild) {
+            this.removeChild(this.firstChild);
+          }
+          return template;
+        }
+        return this.__template;
+      }
+
+      get value() {
+        return Array.isArray(this.__value)
+          ? this.__value.slice()
+          : this.__value || [null];
+      }
+      set value(value) {
+        // console.log('[set values]:', value);
+        if (compare(this.__value, value)) {
+          // console.warn('no change');
+          return;
+        }
+
+        this.__value = parseValues(value);
+
+        this.__updating = true;
+        this.update();
+        setValue(this.__select, this.__value);
+        this.__updating = false;
+      }
+    }
+  );
 
   function onchange(e) {
     if (this.__updating) return;
@@ -225,4 +219,4 @@
     return String(a) === String(b);
   }
 
-})(this);
+})(window);
--- js/src/components/picc-side-meter.js
+++ js/src/components/picc-side-meter.js
@@ -1,20 +1,26 @@
-var CLASS_PREFIX = 'picc-side-meter-';
+(function(exports) {
+  var CLASS_PREFIX = 'picc-side-meter-';
 
-window.PICCMeterStack = document.registerElement('picc-side-meter', {
-  prototype: Object.create(
-    HTMLElement.prototype,
-    {
+  var HTMLParsedElement = require('./html-parsed-element');
 
-      attachedCallback: {value: function() {
+  exports.customElements.define(
+    'picc-side-meter',
+    class extends HTMLParsedElement {
+
+      static get observedAttributes() {
+        return ['min', 'max', 'value', 'median'];
+      }
+
+      parsedCallback() {
         this.min = getAttr(this, 'min', 0);
         this.max = getAttr(this, 'max', 1);
         this.value = getAttr(this, 'value');
         /** this.median = getAttr(this, 'median'); */
 
         this.update();
-      }},
+      }
 
-      attributeChangedCallback: {value: function(attr, prev, value) {
+      attributeChangedCallback(attr, prev, value) {
         // pass through attribute settings to
         // properties for min, max, value an
         // median
@@ -26,205 +32,196 @@ window.PICCMeterStack = document.registerElement('picc-side-meter', {
             this[attr] = value;
             return;
         }
-      }},
+      }
 
-      update: {
-        value: function() {
-          var min = this.min;
-          var max = this.max;
-          var value = this.value;
-          /** var median = this.median; */
+      update() {
+        var min = this.min;
+        var max = this.max;
+        var value = this.value;
+        /** var median = this.median; */
 
-          var bar = getBar(this);
-          var barVal = getBarVal(this);
+        var bar = getBar(this);
+        var barVal = getBarVal(this);
 
-          if (typeof value !== 'number' || isNaN(value)) {
-            // console.log('bad value:', value);
+        if (typeof value !== 'number' || isNaN(value)) {
+          // console.log('bad value:', value);
 
-            // reset the bar
-            bar.removeAttribute('style');
-            bar.style.setProperty('display', 'none');
+          // reset the bar
+          bar.removeAttribute('style');
+          bar.style.setProperty('display', 'none');
 
-            // reset the barVal
-            barVal.removeAttribute('style');
-            barVal.removeAttribute('flow');
-            barVal.style.setProperty('display', 'none');
+          // reset the barVal
+          barVal.removeAttribute('style');
+          barVal.removeAttribute('flow');
+          barVal.style.setProperty('display', 'none');
 
 
-            // classify and bail
-            classify(this, {
-              'no_data': true,
-              'above_average': false,
-              'below_average': false,
-              'about_average': false
-            });
+          // classify and bail
+          classify(this, {
+            'no_data': true,
+            'above_average': false,
+            'below_average': false,
+            'about_average': false
+          });
 
-          } else {
+        } else {
 
-            classify(this, {no_data: false});
+          classify(this, {no_data: false});
 
-            var scale = function(v) {
-              return (v - min) / (max - min) * 100;
-            };
+          var scale = function(v) {
+            return (v - min) / (max - min) * 100;
+          };
 
-            value = Math.max(min, Math.min(this.value, max));
-            var left = Math.min(value, Math.max(0, min));
-            var right = Math.max(0, value);
-            var scaleRight = scale(right);
-            var fixNum = (max > 1) ? 1 : 0;
+          value = Math.max(min, Math.min(this.value, max));
+          var left = Math.min(value, Math.max(0, min));
+          var right = Math.max(0, value);
+          var scaleRight = scale(right);
+          var fixNum = (max > 1) ? 1 : 0;
 
-            // reset the bar and barVal
-            bar.removeAttribute('style');
-            barVal.removeAttribute('style');
-            barVal.removeAttribute('flow');
+          // reset the bar and barVal
+          bar.removeAttribute('style');
+          barVal.removeAttribute('style');
+          barVal.removeAttribute('flow');
 
-            bar.style.setProperty('left', scale(left) + '%');
-            bar.style.setProperty('right', (100 - scaleRight).toFixed(fixNum) + '%');
+          bar.style.setProperty('left', scale(left) + '%');
+          bar.style.setProperty('right', (100 - scaleRight).toFixed(fixNum) + '%');
 
-            // attach the median value next to the bar chart
-            // and responsively scale it from overflowing bar
-            // when the bar is a little less than full
-            var magicNum = (max > 1) ? 6 : 3;
-            var magicFlow = 85;
-            var sRightVal = (100 - (scaleRight + magicNum) <= magicNum ) ? 100 : scaleRight + magicNum;
-            var sLeftVal = (sRightVal === 100) ? scaleRight - magicNum : scaleRight;
+          // attach the median value next to the bar chart
+          // and responsively scale it from overflowing bar
+          // when the bar is a little less than full
+          var magicNum = (max > 1) ? 6 : 3;
+          var magicFlow = 85;
+          var sRightVal = (100 - (scaleRight + magicNum) <= magicNum ) ? 100 : scaleRight + magicNum;
+          var sLeftVal = (sRightVal === 100) ? scaleRight - magicNum : scaleRight;
 
-            if ( sRightVal < (100 - magicNum) ) {
-              barVal.style.setProperty('left', sLeftVal.toFixed(fixNum) + '%');
+          if ( sRightVal < (100 - magicNum) ) {
+            barVal.style.setProperty('left', sLeftVal.toFixed(fixNum) + '%');
 
-              // flag to remove left property at smaller viewports
-              if ( sRightVal > magicFlow ) {
-                barVal.setAttribute('flow', true);
+            // flag to remove left property at smaller viewports
+            if ( sRightVal > magicFlow ) {
+              barVal.setAttribute('flow', true);
 
-              }
-            } else {
-              barVal.style.setProperty('right', (100 -  sRightVal).toFixed(fixNum) + '%');
-              barVal.style.setProperty('background', 'none');
-              barVal.style.setProperty('color', 'inherit'); // for a11y warning
             }
+          } else {
+            barVal.style.setProperty('right', (100 -  sRightVal).toFixed(fixNum) + '%');
+            barVal.style.setProperty('background', 'none');
+            barVal.style.setProperty('color', 'inherit'); // for a11y warning
+          }
 
 
-            /**if (this.hasAttribute('percentile-range')) {
-              var range = this.getAttribute('percentile-range');
-              var numbers = range.split('..').map(Number);
-              if (numbers.some(isNaN)) {
-                console.warn('invalid percentile-range:', range, numbers);
-                classify(this, {
-                  'above_average': false,
-                  'below_average': false,
-                  'about_average': false
-                });
-              } else {
-                var lo = numbers[0];
-                var hi = numbers[1];
-                classify(this, {
-                  'above_average': value > hi,
-                  'below_average': value < lo,
-                  'about_average': value >= lo && value <= hi
-                });
-              }
-            } else {
+          /**if (this.hasAttribute('percentile-range')) {
+            var range = this.getAttribute('percentile-range');
+            var numbers = range.split('..').map(Number);
+            if (numbers.some(isNaN)) {
+              console.warn('invalid percentile-range:', range, numbers);
               classify(this, {
                 'above_average': false,
                 'below_average': false,
                 'about_average': false
               });
-            } */
+            } else {
+              var lo = numbers[0];
+              var hi = numbers[1];
+              classify(this, {
+                'above_average': value > hi,
+                'below_average': value < lo,
+                'about_average': value >= lo && value <= hi
+              });
+            }
+          } else {
+            classify(this, {
+              'above_average': false,
+              'below_average': false,
+              'about_average': false
+            });
+          } */
 
-          }
-
-          delete this.__timeout;
-
-          this.dispatchEvent(new CustomEvent('update'));
         }
-      },
 
-      min: {
-        get: function() {
-          return this.__min;
-        },
-        set: function(value) {
+        delete this.__timeout;
+
+        this.dispatchEvent(new CustomEvent('update'));
+      }
+
+      get min() {
+        return this.__min;
+      }
+      set min(value) {
           this.__min = number(value, 0);
           deferUpdate(this);
-        }
-      },
+      }
 
-      max: {
-        get: function() {
-          return this.__max;
-        },
-        set: function(value) {
-          this.__max = number(value, 1);
-          deferUpdate(this);
-        }
-      },
+      get max() {
+        return this.__max;
+      }
+      set max(value) {
+        this.__max = number(value, 1);
+        deferUpdate(this);
+      }
 
-      value: {
-        get: function() {
-          return this.__value;
-        },
-        set: function(value) {
-          this.__value = number(value);
-          deferUpdate(this);
-        }
-      },
+      get value() {
+        return this.__value;
+      }
+      set value(value) {
+        this.__value = number(value);
+        deferUpdate(this);
+      }
 
-      /** median: {
-        get: function() {
+      /**get median() {
           return this.__median;
-        },
-        set: function(value) {
-          this.__median = number(value);
-          deferUpdate(this);
-        }
-      } */
+      }
+      set median(value) {
+        this.__median = number(value);
+        deferUpdate(this);
+      }*/
     }
-  )
-});
+  );
 
-function deferUpdate(meter, delay) {
-  if (!delay) delay = 50;
-  if (!meter.__timeout) {
-    meter.__timeout = setTimeout(function() {
-      delete meter.__timeout;
-      meter.update();
-    }, delay);
+
+  function deferUpdate(meter, delay) {
+    if (!delay) delay = 50;
+    if (!meter.__timeout) {
+      meter.__timeout = setTimeout(function() {
+        delete meter.__timeout;
+        meter.update();
+      }, delay);
+    }
   }
-}
 
-function getAttr(node, attr, fallback) {
-  return node.hasAttribute(attr)
-    ? node.getAttribute(attr) || fallback
-    : fallback;
-}
-
-function getBar(meter) {
-  var bar = meter.querySelector('.' + CLASS_PREFIX + 'bar');
-  if (!bar) {
-    bar = meter.appendChild(document.createElement('div'));
-    bar.className = CLASS_PREFIX + 'bar';
+  function getAttr(node, attr, fallback) {
+    return node.hasAttribute(attr)
+      ? node.getAttribute(attr) || fallback
+      : fallback;
   }
-  return bar;
-}
 
-function getBarVal(meter) {
-  var avg = meter.querySelector('.' + CLASS_PREFIX + 'val');
-  if (!avg) {
-    avg = meter.appendChild(document.createElement('span'));
-    avg.className = CLASS_PREFIX + 'val';
+  function getBar(meter) {
+    var bar = meter.querySelector('.' + CLASS_PREFIX + 'bar');
+    if (!bar) {
+      bar = meter.appendChild(document.createElement('div'));
+      bar.className = CLASS_PREFIX + 'bar';
+    }
+    return bar;
   }
-  return avg;
-}
 
-function number(value, fallback) {
-  var num = String(value).length ? +value : NaN;
-  // console.log('number(', value, ') ->', num);
-  if (arguments.length < 2) fallback = NaN;
-  return isNaN(num) ? fallback : num;
-}
-
-function classify(el, classes) {
-  for (var klass in classes) {
-    el.classList[classes[klass] ? 'add' : 'remove'](klass);
+  function getBarVal(meter) {
+    var avg = meter.querySelector('.' + CLASS_PREFIX + 'val');
+    if (!avg) {
+      avg = meter.appendChild(document.createElement('span'));
+      avg.className = CLASS_PREFIX + 'val';
+    }
+    return avg;
   }
-}
+
+  function number(value, fallback) {
+    var num = String(value).length ? +value : NaN;
+    // console.log('number(', value, ') ->', num);
+    if (arguments.length < 2) fallback = NaN;
+    return isNaN(num) ? fallback : num;
+  }
+
+  function classify(el, classes) {
+    for (var klass in classes) {
+      el.classList[classes[klass] ? 'add' : 'remove'](klass);
+    }
+  }
+})(window);
