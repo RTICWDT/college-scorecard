0d7d0260094e6ef918f8f0bb716f41488ee81715
Joel Frederick <jafrederick@rti.org>
2019-09-06 15:28:49 -0700

    Adding progress before refactor.  picc instance is not actually available

--- js/src/components/vue/Test.vue
+++ js/src/components/vue/Test.vue
@@ -1,5 +1,12 @@
 <template>
-    <h1>Hello From Vue</h1>
+    <section>
+        <h1>Hello From Vue</h1>
+        
+        <button type="button" class="button button-compare_schools" data-bind="selected_school" aria-label="Add to Compare" data-school="compare-schools" aria-pressed="false" data-school-id="102049" data-school-name="Samford University" aria-describedby="tip-compare-add">
+            <i class="fa fa-plus-circle tooltip-target"></i>
+            <span class="sr-only">Add to Compare</span>
+        </button>
+    </section>
 </template>
 
 <script>
@@ -7,6 +14,15 @@ export default {
     data(){
         return {
             test_message: "Test Me"
+        }
+    },
+    mounted(){
+        console.log("I have mounted");
+        console.log(picc);
+    },
+    methods:{
+        OnClick: function(){
+            console.log("Button Clicked");
         }
     }
 }
