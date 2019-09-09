2164d39892232aaa6d6251e9e4d7982ecb00feaa
Joel Frederick <jafrederick@rti.org>
2019-09-09 15:17:05 -0700

    Things are working, doing some small clean up for proof of concept

--- js/src/app.js
+++ js/src/app.js
@@ -1,15 +1,6 @@
 import Vue from 'vue';
 import Test from './components/vue/Test.vue';
 
-// window.onload = function () {
-//     new Vue({
-//       el: '#app',
-//       components:{
-//         'test': Test
-//       }
-//     });
-// }
-
     new Vue({
       el: '#app',
       components:{
