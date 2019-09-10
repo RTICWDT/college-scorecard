d7d0c595b4115a08ccd909206b4ed3df3f94ba6a
Joel Frederick <jafrederick@rti.org>
2019-09-10 13:25:13 -0700

    Displaying and updating based on state altered within picc

--- js/src/components/vue/Test.vue
+++ js/src/components/vue/Test.vue
@@ -1,24 +1,25 @@
 <template>
     <section>
         <h1>Hello From Vue</h1>
-        
-        <button type="button" class="button button-compare_schools" data-bind="selected_school" aria-label="Add to Compare" data-school="compare-schools" aria-pressed="false" data-school-id="102049" data-school-name="Samford University" aria-describedby="tip-compare-add">
+        <h2>{{school.id}}</h2>
+        <!-- <button type="button" class="button button-compare_schools" data-bind="selected_school" aria-label="Add to Compare" data-school="compare-schools" aria-pressed="false" data-school-id="102049" data-school-name="Samford University" aria-describedby="tip-compare-add">
             <i class="fa fa-plus-circle tooltip-target"></i>
             <span class="sr-only">Add to Compare</span>
-        </button>
+        </button> -->
     </section>
 </template>
 
 <script>
 export default {
+    props:{
+        school: Object
+    },
     data(){
         return {
             test_message: "Test Me"
         }
     },
     mounted(){
-        console.log("I have mounted");
-        console.log(picc);
     },
     methods:{
         OnClick: function(){
