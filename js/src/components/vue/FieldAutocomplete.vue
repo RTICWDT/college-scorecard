d158d01494e44bd454915daaef3cb792e436bf79
Joel Frederick <jafrederick@rti.org>
2019-10-02 18:04:27 -0700

    Added program data fill based on URL pass.

--- js/src/components/vue/FieldAutocomplete.vue
+++ js/src/components/vue/FieldAutocomplete.vue
@@ -9,22 +9,40 @@
       placeholder="Start typing to search"
       return-object
       autocomplete="off"
-      :value="value"
       v-on:change="$emit('input', cip4.cip4.replace('\.',''))"
     />
   </div>
 </template>
 
 <script>
+import {find} from 'lodash';
+
 export default {
   props: ["value"],
-  data: () => ({
-    isLoading: false,
-    cip4: ''
-  }),
+  data(){
+    return{
+        isLoading: false,
+        cip4:{}
+    }
+  },
   computed: {
     items() {
       return picc.CIP4;
+    }
+  },
+  created(){
+    // Transform value prop to expected component state.
+    this.cip4 = this.mapValuePropToState(this.value);
+  },
+  methods:{
+    // Digest URL value and look up in program index.
+    mapValuePropToState(value){
+      if(value.length != 4){
+        return null;
+      }
+
+      let groomedKey = value.slice(0,2) + '.' + value.slice(2);
+      return find(this.items,{cip4: groomedKey});
     }
   }
 };
