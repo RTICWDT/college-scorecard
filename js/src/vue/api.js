732c1f634415f77c22c6ef26f946569923842245
Joel Frederick <jafrederick@rti.org>
2019-10-16 17:52:08 -0700

    Adding progress so far

--- js/src/vue/api.js
+++ js/src/vue/api.js
@@ -1,15 +1,13 @@
-// Install Axios.
 import axios from 'axios';
 
 const initializeConfig = (baseURL) => {
-  // TODO - Add API key here.
+  // TODO - Add API key here.  An axios bug is preventing it right now.
   return axios.create({
     baseURL: baseURL
   });
 }
 
 const apiGet = function(baseURL,apiKey,uri,params = {}) {
-  // Check for required config
   let apiInstance = initializeConfig(baseURL);
 
   return apiInstance.get(uri, {
@@ -20,6 +18,7 @@ const apiGet = function(baseURL,apiKey,uri,params = {}) {
   });
 }
 
+// TODO - Might want to add params and URI as an object then iterate. It would make this method is a little more versitile.
 const apiGetAll = function(baseURL, apiKey, uri, paramArray){
   let promises = [];
 
