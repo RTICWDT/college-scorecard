6529e02fd3aaf95f5df849764bcdeae38a5585d9
Patrick Brown <brownpl@gmail.com>
2019-10-16 21:06:06 -0700

    Hiding some canned searches that don't work

--- js/src/components/vue/CannedSearchContainer.vue
+++ js/src/components/vue/CannedSearchContainer.vue
@@ -14,7 +14,7 @@
           @canned-search-toggle="handleCannedToggle"
         ></canned-search-slider>
       </v-col>
-      <v-col md="4" sm="12" cols="12" class="text-center canned-search-wrapper py-0 my-0">
+      <!-- <v-col md="4" sm="12" cols="12" class="text-center canned-search-wrapper py-0 my-0">
         <canned-search-slider
           label="I want an Associate's Degree"
           :add-to-query="[{degree:'a'}]"
@@ -25,7 +25,7 @@
           :add-to-query="[{degree:'b'}]"
           @canned-search-toggle="handleCannedToggle"
         ></canned-search-slider>
-      </v-col>
+      </v-col> -->
       <v-col md="4" cols="12" class='py-0 my-0'>
         <canned-search-slider
           label="Most People Graduate"
