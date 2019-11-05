ea37b1bf4b01c7bb7c89183a562eb3a12c891a27
Joel Frederick <jafrederick@rti.org>
2019-11-05 13:33:33 -0800

    Added search form to compare page.  Added form to show when no schools are selected.

---
+++ __tests/PageCompare_test.js
@@ -0,0 +1,15 @@
+
+Feature('Page: Comapre');
+
+// Page loads with no console errors
+
+// Visit page with no items in compare
+
+// Visit alternate page, add compare items, visit compare, no errors, see items
+
+
+
+
+Scenario('test something', (I) => {
+
+});
