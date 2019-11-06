85318061d0979ac21326f152aef5a29143808fbe
Joel Frederick <jafrederick@rti.org>
2019-11-06 10:14:26 -0800

    Fixed local storage helper.

--- __tests/PageCompare_test.js
+++ __tests/PageCompare_test.js
@@ -1,16 +1,8 @@
 const assert = require('assert');
 const BASE_URL = "/compare/";
 
-// Used during tests
-const SCHOOL_URL_ARRAY = [
-  '/school/?166027-Harvard-University',
-  '/school/?130794-Yale-University',
-  '/school/?243744-Stanford-University'
-];
-
 Feature('Page: Comapre');
 
-// Page loads with no console errors
 Scenario('Page renders search form with no compare items, no console errors', async (I) => {
   I.amOnPage(BASE_URL);
 
@@ -25,17 +17,15 @@ Scenario('Add items to local storage, see chart elements, expand all, no console
   // Add items to compare;
   I.amOnPage(BASE_URL);
   
-  I.executeScript(() => {
-    localStorage.setItem('compare-schools',JSON.stringify([
-      {
-        schoolName: "Yale University",
-        schoolId: "130794"
-      },{
-        schoolName: "Harvard-University",
-        schoolId: "166027"
-      }
-    ]));
-  });
+  I.fillLocalStorage('compare-schools',[
+    {
+      schoolName: "Yale University",
+      schoolId: "130794"
+    },{
+      schoolName: "Harvard-University",
+      schoolId: "166027"
+    }
+  ]);
 
   // Refresh to pick up new local storage
   I.refreshPage();
@@ -44,6 +34,7 @@ Scenario('Add items to local storage, see chart elements, expand all, no console
 
   // Expand all and render charts
   I.click(locate('.v-btn.v-btn--contained').withText('Expand All'));
+  I.wait(2);
 
   // See no errors
   let errors = await I.grabAndFilterConsole('error');
--- localstorage_helper.js
+++ localstorage_helper.js
@@ -4,26 +4,13 @@ const puppeteer = require('puppeteer');
 
 class LocalStorage extends Helper {
   
-  async fillLocalStorage(key,fillArray = []) {
+  fillLocalStorage(key,fillArray = []) {
     const { page } = this.helpers.Puppeteer;
     
-    let test = await page.evaluate((key,fillArray)=>{
-      // return fillArray;
-      fillArray.forEach(element => {
-        localStorage.setItem(key,JSON.stringify(element));  
-      });
-    },key, fillArray)
-    .catch(e => {
-      console.log("Error: " + JSON.stringify(e));
-    })
-
-    console.log("key: " + JSON.stringify(test));
+    page.evaluate((key,fillArray)=>{
+      localStorage.setItem(key,JSON.stringify(fillArray));
+    },key, fillArray);
   }
-
-  addToLocalStorage(key,value){
-
-  }
-
 }
 
 module.exports = LocalStorage;
