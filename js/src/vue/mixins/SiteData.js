d5409c2e4e95e4122ad90ec1ba27c5a8a991282a
Joel Frederick <jafrederick@rti.org>
2019-11-07 12:11:19 -0800

    Added data digestion via js imports, removed old data files, removed call to import site data.

--- js/src/vue/mixins/SiteData.js
+++ js/src/vue/mixins/SiteData.js
@@ -1,11 +1,13 @@
+import cip_2_digit from "../../../../_data/cip_2_digit.json";
+import cip_4_digit from "../../../../_data/cip_4_digit.json";
+import glossary from "../../../../_data/glossary.json";
+import race_ethnicity from "../../../../_data/race_ethnicity.json";
+import religious_affiliations from "../../../../_data/religious_affiliations.json";
+import special_designations from "../../../../_data/special_designations.json";
+import states from "../../../../_data/states.json";
+
 export const SiteData = {
   computed:{
-    PROGRAM_NAMES(){
-      return this.site.data.programs.reduce( function(object,value){
-        object[value.key] = value.label;
-        return object;
-      }, {});
-    },
     RELIGIOUS_AFFILIATIONS_BY_NUMBER(){
       return this.site.data.religious_affiliations.reduce( function(object,value){
         object[value.value] = value.label;
@@ -28,7 +30,15 @@ export const SiteData = {
   data(){
     return {
       site:{
-        data: siteDataAll
+        data: {
+          cip_2_digit: cip_2_digit,
+          cip_4_digit: cip_4_digit,
+          glossary: glossary,
+          race_ethnicity: race_ethnicity,
+          religious_affiliations: religious_affiliations,
+          special_designations: special_designations,
+          states: states
+        }
       }
     }
   }
