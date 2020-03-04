d1a29a50f76ce02323daf04c8258aba4ae8bd984
Joel Frederick <jafrederick@rti.org>
2020-03-04 15:58:25 -0800

    Testing new test url format

--- test/url.js
+++ test/url.js
@@ -10,7 +10,8 @@ if (branch === 'master') {
   url = 'https://collegescorecard.ed.gov';
 } else if (branch && user && repo) {
   var path = [user, repo, branch].join('/');
-  url = 'https://federalist-proxy.app.cloud.gov/preview/' + path.toLowerCase();
+  // url = 'https://federalist-proxy.app.cloud.gov/preview/' + path.toLowerCase();
+  url = env.BASEURL + path.toLowerCase();
 }
 
 if (module.parent) {
