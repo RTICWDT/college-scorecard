2c97aef56677879d3706ecb96276940ee8ee5647
Joel Frederick <jafrederick@rti.org>
2020-03-04 17:04:57 -0800

    Removing test URL

--- test/url.js
+++ test/url.js
@@ -10,8 +10,7 @@ if (branch === 'master') {
   url = 'https://collegescorecard.ed.gov';
 } else if (branch && user && repo) {
   var path = [user, repo, branch].join('/');
-  // url = 'https://federalist-proxy.app.cloud.gov/preview/' + path.toLowerCase();
-  url = env.BASEURL + path.toLowerCase();
+  url = 'https://federalist-proxy.app.cloud.gov/preview/' + path.toLowerCase();
 }
 
 if (module.parent) {
