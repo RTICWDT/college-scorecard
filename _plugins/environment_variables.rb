8bb661b10ea7384db9dc506feb6c0570421ae438
Joel Frederick <jafrederick@rti.org>
2020-03-25 17:22:53 -0700

    Testing env passoff to generate google analytics file.

--- _plugins/environment_variables.rb
+++ _plugins/environment_variables.rb
@@ -20,6 +20,10 @@ module Jekyll
       if ENV['API_KEY_GOOGLE_MAPS']
         site.config['api-key-google-maps'] = ENV['API_KEY_GOOGLE_MAPS']
       end
+      if ENV['ANALYTICS_STRING']
+        puts "Adding google auth string"
+        File.write("_site/analytics.txt", ENV['ANALYTICS_STRING'])
+      end
       # puts "API: ------------------"
       # puts site.config['API']
       # puts "------------------"
