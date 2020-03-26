b9e42e1793b3f53eb6a1c250db119e70dc0fff02
Joel Frederick <jafrederick@rti.org>
2020-03-25 17:24:55 -0700

    Adding comment

--- _plugins/environment_variables.rb
+++ _plugins/environment_variables.rb
@@ -20,6 +20,7 @@ module Jekyll
       if ENV['API_KEY_GOOGLE_MAPS']
         site.config['api-key-google-maps'] = ENV['API_KEY_GOOGLE_MAPS']
       end
+      # Pass off analytics string
       if ENV['ANALYTICS_STRING']
         puts "Adding google auth string"
         File.write("_site/analytics.txt", ENV['ANALYTICS_STRING'])
