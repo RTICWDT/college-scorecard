670799a4b9957fe534da462a81e79346f9a31a08
Joel Frederick <jafrederick@rti.org>
2020-03-27 16:17:45 -0700

    Final adjustments on hound

--- _plugins/analytics_string.rb
+++ _plugins/analytics_string.rb
@@ -9,8 +9,8 @@ module Jekyll
         site.config['analytics-string'] = ENV['ANALYTICS_STRING']
       end
 
-      File.write('analytics.txt',site.config['analytics-string'])
-      site.static_files << Jekyll::StaticFile.new(site,site.source,'/','analytics.txt')
+      File.write('analytics.txt', site.config['analytics-string'])
+      site.static_files << Jekyll::StaticFile.new(site, site.source, '/', 'analytics.txt')
     end
   end
 end
