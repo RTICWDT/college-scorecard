c3bf1a1a638b7ffe966f154e55c69179ff0763bf
Joel Frederick <jafrederick@rti.org>
2020-03-27 12:16:06 -0700

    Ruby handoff

---
+++ _plugins/analytics_string.rb
@@ -0,0 +1,22 @@
+module Jekyll
+
+  class AnalyticsGenerator < Generator
+    
+    # include StaticFile
+
+    def generate(site)
+
+      puts "Adding google auth string"
+
+      if ENV['ANALYTICS_STRING']
+        site.config['analytics-string'] = ENV['ANALYTICS_STRING']
+      end
+
+      File.write("analytics.txt", site.config['analytics-string'])
+      site.static_files << Jekyll::StaticFile.new(site, site.source, "/" , "analytics.txt")
+      
+    end
+  
+  end
+
+end
