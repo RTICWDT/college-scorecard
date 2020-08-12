9fb84cb3f523f02e05f3bf72c6a41f2c73c4cb13
Joel Frederick <jafrederick@rti.org>
2020-08-12 15:55:49 -0700

    Added styling for compare drawer

--- js/src/components/vue/CompareDrawer.vue
+++ js/src/components/vue/CompareDrawer.vue
@@ -1,32 +1,63 @@
 <template>
-  <v-card id="compare_schools-content" class="pa-5">
+  <v-card id="compare-schools-content" class="pa-5">
     <v-btn icon class="float-right" @click="toggleDrawer()">
       <v-icon>fas fa-times-circle</v-icon>
     </v-btn>
     <p>Add up to 10 Schools and 10 Fields of Study to compare.</p>
-    <v-row>
-      <v-col cols="12" md="6">
-        <p class="title">Compare Schools</p>
+    <v-row style="height: 100%">
+      <v-col cols="12" md="5">
+        <h3 class="title">Compare Schools</h3>
         <div class="my-3">
+<!--          <v-checkbox-->
+<!--            @change="handleToggleCompareItem(school,schoolKey)"-->
+<!--            :label="school.schoolName"-->
+<!--            v-for="school in schools"-->
+<!--            :key="school.schoolId"-->
+<!--            :value="school.schoolId"-->
+<!--            hide-details-->
+<!--            v-model="selectedSchools"-->
+<!--            color="secondary"-->
+<!--            class="ma-0 pa-0"-->
+<!--          >-->
+<!--            -->
+<!--          </v-checkbox>-->
+
           <v-checkbox
             @change="handleToggleCompareItem(school,schoolKey)"
-            :label="school.schoolName"
             v-for="school in schools"
             :key="school.schoolId"
             :value="school.schoolId"
             hide-details
             v-model="selectedSchools"
             color="secondary"
-            class="ma-0 pa-0"
-          ></v-checkbox>
+            class="my-5 pa-0"
+          >
+            <template v-slot:label>
+              <h4>
+                {{school.schoolName}}
+              </h4>
+            </template>
+          </v-checkbox>
+
         </div>
-        <p>
-          <v-btn rounded color="secondary" :href="$baseUrl+'/compare'">Compare Schools</v-btn>
-        </p>
+
+        <!-- Medium and smaller button-->
+        <div class="d-md-none mt-5 text-center">
+          <v-btn
+            rounded
+            color="secondary"
+            :href="$baseUrl+'/compare'"
+          >
+            Compare Schools
+          </v-btn>
+        </div>
+
       </v-col>
-      <v-col cols="12" md="6">
-        <p class="title">Compare Fields Of Study</p>
-        <div class="my-3">
+
+      <v-col cols="12" md="7" id="compare-drawer-fos-container">
+        <h3 class="title compare-drawer-fos-title">Compare Fields Of Study</h3>
+
+        <div class="my-3 compare-drawer-fos-checkbox-container">
           <v-checkbox
             v-for="fieldOfStudy in fieldsOfStudy"
             @change="handleToggleCompareItem(fieldOfStudy, fieldOfStudyKey)"
@@ -35,28 +66,100 @@
             v-model="selectedFieldsOfStudy"
             :value="generateFieldOfStudyString(fieldOfStudy)"
             color="secondary"
-            class="ma-0 pa-0"
+            class="my-5"
           >
             <template v-slot:label>
-              <div>
-                <span>{{fieldOfStudy.fosTitle}}</span><br>
-                <span>{{fieldOfStudy.credentialTitle}}</span><br>
-                <span>{{fieldOfStudy.institutionName}}</span>
+              <div class="">
+                <h4>{{fieldOfStudy.fosTitle | formatCip2Title}}</h4>
+                <h5>{{fieldOfStudy.credentialTitle}}</h5>
+                <h5>{{fieldOfStudy.institutionName}}</h5>
               </div>
             </template>
           </v-checkbox>
         </div>
-        <p>
-          <v-btn rounded color="secondary" :href="$baseUrl+'/compare/?toggle=fos'">Compare Fields Of Study</v-btn>
-        </p>
+
+        <!-- Medium and smaller button-->
+        <div class="d-md-none mt-5 text-center">
+          <v-btn class="compare-drawer-button"
+                 rounded
+                 color="secondary"
+                 :href="$baseUrl+'/compare/?toggle=fos'"
+          >
+            Compare Fields Of Study
+          </v-btn>
+        </div>
+
       </v-col>
     </v-row>
+
+    <v-row>
+      <v-col cols="12" md="5" class="text-center d-none d-md-block" >
+        <v-btn
+          rounded
+          color="secondary"
+          :href="$baseUrl+'/compare'"
+        >
+          Compare Schools
+        </v-btn>
+      </v-col>
+      <v-col cols="12" md="7" class="text-center compare-drawer-button-container d-none d-md-block">
+        <v-btn class="compare-drawer-button"
+               rounded
+               color="secondary"
+               :href="$baseUrl+'/compare/?toggle=fos'"
+        >
+          Compare Fields Of Study
+        </v-btn>
+      </v-col>
+    </v-row>
+
   </v-card>
 </template>
+
 <style lang="scss" scoped>
-.v-input--selection-controls.v-input .v-label {
-  color: #ffffff !important;
-}
+  @import 'sass/_variables';
+  .v-input--selection-controls.v-input .v-label {
+    color: #ffffff !important;
+  }
+
+  #compare-drawer-fos-container{
+    min-height: 400px;
+  }
+
+  .compare-drawer-fos-title{
+    @media (min-width: 960px) {
+      padding-left: 24px;
+    }
+  }
+
+  .compare-drawer-button-container{
+
+  }
+
+  .compare-drawer-fos-checkbox-container{
+
+
+
+    @media (min-width: 960px){
+      padding-left: 24px;
+      border-left: 1px solid $light-gray;
+      min-height: 420px;
+      /*height: 100%;*/
+    }
+  }
+
+  #compare-schools-content{
+    @media (min-width: 960px){
+      min-height: 500px;
+    }
+
+  }
+
+  .compare-drawer-button{
+    /*position: absolute;*/
+    /*bottom: 24px;*/
+  }
+
 </style>
 <script>
 // This can work on any page, it just needs data passed in and events to react when school
