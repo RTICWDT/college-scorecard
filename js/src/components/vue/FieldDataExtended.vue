3dfc7c91aab7ed6bb63ed7b6580df89a13cf3a3e
Joel Frederick <jafrederick@rti.org>
2020-08-18 08:54:27 -0700

    refining detail component

--- js/src/components/vue/FieldDataExtended.vue
+++ js/src/components/vue/FieldDataExtended.vue
@@ -1,7 +1,12 @@
 <template>
   <div class='fos-field-data-container'>
-    <h3 class="grey lighten-4 pa-2">Salary After Completing</h3>
-    <v-row>
+
+    <h3 class="grey lighten-4 pa-3 mt-2">
+      Salary After Completing
+    </h3>
+
+    <!-- Salary After Completing-->
+    <v-row class="mx-sm-1">
       <v-col cols="12" md="5">
         <v-select
           hide-details
@@ -13,8 +18,8 @@
 
       <!--Median Earnings-->
       <v-col cols="12" md="3">
-        <h4 class="mb-2">
-          Median Earnings&nbsp
+        <h4 class="mb-2 field-data-header-title">
+          Median <br class="d-none d-md-block">Earnings&nbsp
           <tooltip definition="fos-number-of-graduates" :limitedFoS="fieldsLink" />
         </h4>
 
@@ -22,8 +27,9 @@
           <div v-if="_.get(fos, fields.FOS_EARNINGS_FED)">
             <span class="fos-field-data-text">{{_.get(fos, fields.FOS_EARNINGS_FED) | numeral('$0,0') }}</span>
           </div>
-          <div v-else>
-            <span class="fos-field-data-text">N/A</span>
+
+          <div v-else class="mini-data-na text-center">
+            Data Not Available
           </div>
 
         </div>
@@ -32,16 +38,17 @@
           <div v-if="_.get(fos, fields.FOS_EARNINGS_PELL)">
             <span class="fos-field-data-text">{{_.get(fos, fields.FOS_EARNINGS_PELL) | numeral('$0,0') }}</span>
           </div>
-          <div v-else>
-            <span class="fos-field-data-text">N/A</span>
+
+          <div v-else class="mini-data-na text-center">
+            Data Not Available
           </div>
         </div>
       </v-col>
 
       <!--Monthly Earnings-->
-      <v-col cols="12" md="3">
+      <v-col cols="12" md="4">
         <h4 class="mb-2">
-          Monthly Earnings&nbsp
+          Monthly <br class="d-none d-md-block">Earnings&nbsp
           <tooltip definition="fos-number-of-graduates" :limitedFoS="fieldsLink" />
         </h4>
 
@@ -50,8 +57,8 @@
             <span class="fos-field-data-text">{{_.get(fos, fields.FOS_EARNINGS_FED) / 12 | numeral('$0,0') }}</span>
           </div>
 
-          <div v-else>
-            <span class="fos-field-data-text">N/A</span>
+          <div v-else class="mini-data-na text-center">
+            Data Not Available
           </div>
 
         </div>
@@ -61,15 +68,20 @@
             <span class="fos-field-data-text">{{_.get(fos, fields.FOS_EARNINGS_PELL) / 12 | numeral('$0,0') }}</span>
           </div>
 
-          <div v-else>
-            <span class="fos-field-data-text">N/A</span>
+          <div v-else class="mini-data-na text-center">
+            Data Not Available
           </div>
+
         </div>
       </v-col>
     </v-row>
 
-    <h3 class="grey lighten-4 pa-2">Financial Aid &amp; Debt</h3>
-    <v-row>
+    <!-- Financial Aid &amp; Debt-->
+    <h3 class="grey lighten-4 pa-3">
+      Financial Aid &amp; Debt
+    </h3>
+
+    <v-row class="mx-sm-1">
       <v-col cols="12" md="5">
         <v-checkbox
           hide-details
@@ -88,7 +100,7 @@
       <!--Median Total-->
       <v-col cols="12" md="3">
         <h4 class="mb-2">
-          Median Total Debt After Graduation&nbsp
+          Median Total <br class="d-none d-md-block">Debt After Graduation&nbsp
           <tooltip definition="fos-number-of-graduates" :limitedFoS="fieldsLink" />
         </h4>
 
@@ -97,8 +109,8 @@
             <span class="fos-field-data-text">{{_.get(fos, fields.FOS_DEBT_MEDIAN) | numeral('$0,0') }}</span>
           </div>
 
-          <div v-else>
-            <span class="fos-field-data-text">N/A</span>
+          <div v-else class="mini-data-na text-center">
+            Data Not Available
           </div>
         </div>
 
@@ -107,8 +119,8 @@
             <span class="fos-field-data-text">{{_.get(fos, fields.FOS_DEBT_MEDIAN_PRIOR) | numeral('$0,0') }}</span>
           </div>
 
-          <div v-else>
-            <span class="fos-field-data-text">N/A</span>
+          <div v-else class="mini-data-na text-center">
+            Data Not Available
           </div>
         </div>
       </v-col>
@@ -125,8 +137,8 @@
             <span class="fos-field-data-text">{{_.get(fos,fields.FOS_DEBT_MONTHLY) | numeral('$0,0') }}</span>
           </div>
 
-          <div v-else>
-            <span class="fos-field-data-text">N/A</span>
+          <div v-else class="mini-data-na text-center">
+            Data Not Available
           </div>
         </div>
 
@@ -135,17 +147,19 @@
             <span class="fos-field-data-text">{{_.get(fos, fields.FOS_DEBT_MONTHLY_PRIOR) | numeral('$0,0') }}</span>
           </div>
 
-          <div v-else>
-            <span class="fos-field-data-text">N/A</span>
+          <div v-else class="mini-data-na text-center">
+            Data Not Available
           </div>
         </div>
 
       </v-col>
     </v-row>
 
-    <h3 class="grey lighten-4 pa-2">Additional Information</h3>
-    <v-row>
+    <h3 class="grey lighten-4 pa-3">
+      Additional Information
+    </h3>
 
+    <v-row class="mx-sm-1">
       <!--Number Of Graduates-->
       <v-col cols="12" md="5">
         <h4 class="mb-2">
@@ -155,8 +169,9 @@
          <div v-if="_.get(fos, fields.FOS_GRAD_COUNT)">
            <span class="fos-field-data-text">{{_.get(fos, fields.FOS_GRAD_COUNT)}}</span>
         </div>
-        <div v-else>
-          <span class="fos-field-data-text">N/A</span>
+
+        <div v-else class="mini-data-na text-center">
+          Data Not Available
         </div>
 
       </v-col>
@@ -170,10 +185,10 @@
   @import 'sass/_variables';
 
   .fos-field-data-container{
-    padding: 4px 0px;
+    /*padding: 4px 0px;*/
 
     @media (min-width: 960px) {
-      padding: 10px;
+      /*padding: 10px;*/
     }
 
   }
