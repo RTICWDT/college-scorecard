e7e765dc2837a3d502d27deb22b674a56b1672b7
Patrick Brown <brownpl@gmail.com>
2020-11-30 14:22:41 -0800

    Update config.yml
    Updating apt prior to installing packages.
    
    Update config.yml
    
    Tweak to get tests to run.
    
    Update url.js
    
    Adding additional preview URL
    
    Update url.js
    
    Tweak to repository name.
    
    Update url.js
    
    Debugging repo.
    
    Update url.js
    
    Updating repository URL
    
    Update url.js
    
    Tweak to trigger tests
    
    Update url.js
    
    Please run, tests!
    
    Update url.js
    
    Trigger test
    
    Update url.js
    
    Debug env vars for circle ci
    
    Update url.js
    
    Update url.js
    
    Fixed repo name error.
    
    Update url.js
    
    Fixing URL in another file.
    
    Update config.yml
    
    Refactoring federalist check.
    
    Update url.js
    
    Adding sub directory.
    
    Update url.js

--- __tests__/utility/url.js
+++ __tests__/utility/url.js
@@ -10,7 +10,14 @@ if (branch === 'master') {
   url = 'https://collegescorecard.ed.gov';
 } else if (branch && user && repo) {
   var path = [user, repo, branch].join('/');
-  url = 'https://federalist-c15eda78-ad4f-46bc-ac6b-eb043d9f3780.app.cloud.gov/preview/' + path.toLowerCase();
+  if(repo=='private-scorecard')
+  {
+    url = 'https://federalist-a267b602-ea49-4771-8115-ea94f984a5ca.app.cloud.gov/preview/' + path.toLowerCase();
+  }
+  else
+  {
+    url = 'https://federalist-c15eda78-ad4f-46bc-ac6b-eb043d9f3780.app.cloud.gov/preview/' + path.toLowerCase();
+  }
 }
 
 if (module.parent) {
