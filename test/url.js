dc9b041d512d99b9438498f5047abffeda610a95
Patrick Brown <brownpl@gmail.com>
2020-11-30 18:44:41 -0800

    Update url.js

--- test/url.js
+++ test/url.js
@@ -10,7 +10,7 @@ if (branch === 'master') {
   url = 'https://collegescorecard.ed.gov';
 } else if (branch && user && repo) {
   var path = [user, repo, branch].join('/');
-  if(repo.match('private-scorecard'))
+  if(repo==='private-scorecard')
   {
     url = 'https://federalist-d623c0f7-8399-4159-ab13-eaae6047be2e.app.cloud.gov/preview/' + path.toLowerCase();
   }
@@ -19,7 +19,6 @@ if (branch === 'master') {
     url = 'https://federalist-c15eda78-ad4f-46bc-ac6b-eb043d9f3780.app.cloud.gov/preview/' + path.toLowerCase();
   }
 }
-
 
 if (module.parent) {
   if (env.CIRCLE_PR_USERNAME && env.CIRCLE_PR_USERNAME != user) {
