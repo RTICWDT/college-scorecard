d14a2c2843b090b403f086612aaad434e5f4eb4f
Patrick Brown <brownpl@gmail.com>
2020-11-30 16:44:16 -0800

    Update url.js
    Adding additional preview URL

--- test/url.js
+++ test/url.js
@@ -10,7 +10,14 @@ if (branch === 'master') {
   url = 'https://collegescorecard.ed.gov';
 } else if (branch && user && repo) {
   var path = [user, repo, branch].join('/');
-  url = 'https://federalist-c15eda78-ad4f-46bc-ac6b-eb043d9f3780.app.cloud.gov/preview/' + path.toLowerCase();
+  if(repo=='private-scorecard')
+  {
+    url = 'https://federalist-d623c0f7-8399-4159-ab13-eaae6047be2e.app.cloud.gov/' + path.toLowerCase();
+  }
+  else
+  {
+    url = 'https://federalist-c15eda78-ad4f-46bc-ac6b-eb043d9f3780.app.cloud.gov/preview/' + path.toLowerCase();
+  }
 }
 
 if (module.parent) {
