d48a16c4544f97a48d5f44dc098b75a1acd8fd87
Alex Smith <alexsmith@rti.org>
2020-12-10 11:37:22 -0500

    added missing file

---
+++ js/src/components/vue/RepaymentRate.vue
@@ -0,0 +1,247 @@
+<template>
+  <div>
+    <div v-show="has_data" class="om_visualization">         
+      <div class="om_sankey" ref="sankey"></div>
+ 
+    </div>
+
+    <div v-show="!has_data" class="data-na">
+      <div class="om_sankey na">Data not available.</div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+@import "sass/_variables.scss";
+  
+
+.om_visualization{
+  display: flex;
+  clear:both;
+  justify-content: center;
+  .om_group{
+    width: 0%;
+    text-align: center;
+    padding-top: 0px;
+    padding-right: $base-padding;
+  }
+  .om_sankey{
+    width: 100%;
+    height: 300px;
+    margin-top: $base-padding;
+    padding-left: $base-padding;
+
+    text[text-anchor="end"]{
+      display:none;
+    }
+    path[fill="#ffffff"]{
+      display:none;
+    }
+    rect[fill="#ffffff"]{
+      display:none;
+    }
+    rect{
+      fill-opacity: 1 !important;
+    }
+  }
+  .google-visualization-tooltip{
+    display:none;
+  }
+}
+.na{
+  height: 300px;
+}
+
+</style>
+
+
+<script>
+export default {
+  props: {
+    school: Object,
+    colors: {
+      type: String,
+      default: "multi"
+    },
+    gradOnly:{
+      type: Boolean,
+    }
+  },
+  data() {
+    return {
+      outcomes: Object,
+      outcome_cohorts: Object,
+      has_data: true
+    };
+  },
+  computed:{
+    showGradOnly(){
+      return this.gradOnly;
+    }
+  },
+
+  mounted() {
+    this.extractData();
+    google.charts.load("current", {
+      packages: ["corechart", "table", "sankey"],
+      callback: this.drawSankeyChart
+    });
+    window.addEventListener("resize", this.drawSankeyChart);
+  },
+  watch:{
+    showGradOnly: function(){ this.drawSankeyChart(); }
+  },
+
+  methods: {
+    extractData() {
+      this.outcomes = _.get(
+        this.school,
+        "latest.completion.outcome_percentage_suppressed"
+      );
+
+      var outcomesString = `{
+        "graduates": {
+            "paid_in_full": 0.25,
+            "making_progress": 0.25,
+            "not_making_progress": 0.035,
+            "deferment": 0.1000,
+            "deqlinquent": 0.0500,
+            "defaulted": 0.0500,
+            "discharged": 0.0195,
+            "forebearance": null
+            },
+        "all": {
+            "paid_in_full": 0.0300,
+            "making_progress": 0.0105,
+            "not_making_progress": 0,
+            "deferment": 0.1000,
+            "deqlinquent": 0.100,
+            "defaulted": 0.3505,
+            "discharged": 0.3500,
+            "forebearance": 0.0395
+            }
+      }`
+      this.outcomes = JSON.parse(outcomesString);
+    },
+
+    drawSankeyChart() {
+      var links = {
+        graduates: {
+            variable: "graduates",
+            text:
+              "Out of students who started college here and started their studies full-time..."
+        },
+        all: {
+            variable: "all",
+            text:
+              "Out of students who started college here and started their studies full-time..."
+        }        
+      };
+
+      var friendlyMetrics = {
+        paid_in_full: "Paid In Full",
+        making_progress: "Making Progress",
+        not_making_progress: "Not Making Progress",
+        deferment: "Deferment",
+        deqlinquent: "Delinquent",
+        defaulted: "Defaulted",
+        discharged: "Discharged",
+        forebearance: "Forebearance",        
+      };
+
+      var metricVariable = (this.gradOnly ? links.graduates.variable : links.all.variable);
+      var currentData = _.get(
+        this.outcomes,
+        metricVariable
+      );
+      var rows = [];
+      var percent;
+     
+      for (var q in currentData) {
+        percent = Math.round(currentData[q] * 100);
+        if (percent > 0) {
+          rows.push([percent + "% " + friendlyMetrics[q], "Group", percent]);
+        }
+        else if (percent == 0) {
+          rows.push([percent + "% " + friendlyMetrics[q], "Group", 1]);
+        }        
+        else if (!percent) {
+          rows.push(["NA - " + friendlyMetrics[q], "Group", 2]);
+        }
+      }
+
+      if (rows.length > 0) {
+        this.has_data = true;
+        var data = new google.visualization.DataTable();
+        var colors = [];
+        data.addColumn("string", "From");
+        data.addColumn("string", "To");
+        data.addColumn("number", "Percent");
+        data.addRows(rows);
+        if (this.colors == "solid") {
+          colors = [
+            "#0e365b",
+            "#FFFFFF",
+            "#0e365b",
+            "#0e365b",
+            "#0e365b",
+            "#0e365b",
+            "#0e365b",
+            "#0e365b",
+            "#0e365b",            
+          ];
+        } else {
+          colors = [
+            "#d37c39",
+            "#FFFFFF",
+            "#86B33B",
+            "#8360ED",
+            "#49ACEC",
+            "#37608D",
+            "#49ACEC",
+            "#37608D"            
+          ];
+        }
+
+        // Sets chart options.
+        var options = {
+          width: "100%",
+          tooltip: {
+            trigger: "selection",
+            isHtml: true
+          },
+          sankey: {
+            node: {
+              colors: colors,
+              width: 80,
+              //nodePadding: 0,
+              label: {
+                fontName:
+                  'Public Sans,"Helvetica Neue",Helvetica,arial,sans-serif',
+                fontSize: 14,
+                color: "#040404"
+              },
+              interactivity: false,
+              colorMode: "unique"
+            },
+            link: {
+              color: "#FFFFFF"
+            },
+            tooltip: false
+          }
+        };
+        var chart = new google.visualization.Sankey(this.$refs["sankey"]);
+
+        // delay this so the height can get setup before the 
+        // chart is drawn when transitioning from no data
+        setTimeout(function(){
+          chart.draw(data, options);
+        }, 100);
+
+      } else {
+        this.has_data = false;
+      }
+    }
+  }
+};
+</script>
