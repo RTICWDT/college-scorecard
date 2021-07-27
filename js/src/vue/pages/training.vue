7cbec8fa75896cda0a7573faa5c6fcb8ea954cbc
Alex Smith <alexsmith@rti.org>
2021-07-27 13:34:17 -0400

    landing page edits done

--- js/src/vue/pages/training.vue
+++ js/src/vue/pages/training.vue
@@ -8,13 +8,13 @@
     <div class="justify-center text-center" style="background:#fff;">
       
       <div class="text-center justify-center ms-8 py-4" style="width: max-content; margin: auto !important;"> 
-        <img :src="`${$baseUrl}/img/icon-Note.svg`" alt="Note Icon" style="vertical-align: middle;"/>  <span>The College Scorecard shows all schools and programs that qualify for Pell Grants <tooltip definition="wioa-participants"/></span>
+        <img :src="`${$baseUrl}/img/icon-Note.svg`" alt="Note Icon" style="vertical-align: middle;"/>  <span>The College Scorecard shows all schools and programs that qualify for Pell Grants <tooltip definition="pell-grant"/></span>
       </div>        
       </div>
       <!-- Top Splash and Search-->
       <div class="home-splash">
         <v-container class="pt-0">
-          <v-row>
+          <v-row class="pb-16">
             <!-- Header Info -->
             <v-col cols="2"></v-col>
             <v-col cols="8">
@@ -40,7 +40,7 @@
                <v-container>
                  <v-row align="center">
                    <v-col cols="12" md="6" sm="12">
-                     <h3 class="text-left pb-2">Schools with qualifying programs:<tooltip definition="wioa-participants"/></h3>
+                     <h3 class="text-left pb-2">Schools with qualifying programs<tooltip definition="wioa-participants"/></h3>
                      <p>Search nearby schools that accept Pell Grants. These colleges also have eligible training programs for participants in Workforce Innovation and Opportunity Act (WIOA) programs, which are funded by the U.S. Department of Labor and operated by state and local grantees.</p>
                    </v-col>
                    <v-col cols="12" md="6" sm="12" align="center">
@@ -51,9 +51,8 @@
                         large
                         color="secondary"
                         @click="handleSearchNearbySchools()"
-                        style="border:2px solid white !important;"
                       >
-                      <img :src="`${$baseUrl}/img/icon-Location.svg`" alt="Department of Labor Icon" class="py-3 pl-3 pr-8"/> SEARCH NEARBY SCHOOLS
+                      <img :src="`${$baseUrl}/img/icon-Location.svg`" alt="Department of Labor Icon" class="py-3 pl-3 pr-1"/> SEARCH NEARBY SCHOOLS
 
                       </v-btn>
                     </p>    
@@ -70,7 +69,7 @@
               class="d-sm-flex pb-10"
             >           
               <v-card class="pa-8" width="100%">
-                <h3 class="text-center pb-2">Search fields of study at schools that have WIOA training programs:<tooltip definition="wioa-participants"/></h3>
+                <h3 class="text-center pb-2">Search fields of study at schools that have WIOA training programs<tooltip definition="wioa-participants"/></h3>
                 <field-of-study-search
                   @field-of-study-selected="handleFieldOfStudySelected"
                 />             
@@ -107,12 +106,12 @@
 
                 </div>
 
-                <h3>
+                <h3 class="pb-5 mx-10">
                   Department of Labor WIOA Program
                 </h3>
 
-                <p>
-                  Department of Labor WIOA Program Some training programs are eligible for funding through the Workforce Innovation and Opportunity Act (WIOA), depending on your state and local needs. Search all training programs that are funded through WIOA on TrainingProviderResults.gov. This Department of Labor tool provides key outcome data on WIOA programs such as training completion, credential attainment, employment and wages.
+                <p class="mx-10">
+                  Some training programs are eligible for funding through the Workforce Innovation and Opportunity Act (WIOA), depending on your state and local needs. Search all training programs that are funded through WIOA on TrainingProviderResults.gov. This Department of Labor tool provides key outcome data on WIOA programs such as training completion, credential attainment, employment and wages.
                 </p>
 
                 <p class="home-callout-button-wrapper text-center">
@@ -120,9 +119,10 @@
                     href="https://trainingproviderresults.gov"
                     target="_blank"
                     @click="trackOutboundLink($event)"
-                  >TRAININGPROVIDERRESULTS.GOV 
+                  >TrainingProviderResults.gov
                     <v-icon
                       x-small
+                      class="pl-1"
                     >
                       fas fa-external-link-alt
                     </v-icon>                  
@@ -140,7 +140,7 @@
               sm="12"
             >
 
-              <div class="home-callout-container ml-md-3">
+              <div class="home-callout-container ml-md-3 px-10">
 
                 <div class="home-callout-top mb-12">
 
@@ -151,12 +151,12 @@
                   </div>
                 </div>
 
-                <h2>
-                  Get started with Financial Aid
-                </h2>
+                <h3 class="pb-5">
+                  Get Started with Financial Aid
+                </h3>
 
                 <p>
-                  Get Started with Financial Aid To receive financial aid, you must complete the Free Application for Federal Student Aid (FAFSA). You also may qualify for a Pell Grant, which is a federal program that provides free money for higher education. In order to see if you qualify for a Pell Grant, you must submit a FAFSA. Use the  
+                  To receive financial aid, you must complete the Free Application for Federal Student Aid (FAFSA). You also may qualify for a Pell Grant, which is a federal program that provides free money for higher education. In order to see if you qualify for a Pell Grant, you must submit a FAFSA. Use the  
                   <a href="https://fafsa.ed.gov/FAFSA/app/f4cForm"
                      target="_blank"
                      @click="trackOutboundLink($event)"
@@ -164,7 +164,7 @@
                     FAFSA<em>4caster</em> 
                     <v-icon
                       x-small
-                      class="green-link"
+                      class="green-link pl-1"
                     >
                       fas fa-external-link-alt
                     </v-icon>                    
@@ -173,7 +173,7 @@
                   to see how much aid may be available for you.
                 </p>
 
-                <p class="text-center home-callout-button-wrapper">
+                <p class="text-center home-callout-button-wrapper white--text">
                   <v-btn
                     block
                     color="secondary"
@@ -184,6 +184,7 @@
                     <v-icon
                       x-small
                       color="white"
+                      class="pl-1"
                     >
                       fas fa-external-link-alt
                     </v-icon>
@@ -221,16 +222,23 @@
 
 <style lang="scss" scoped>
 @import 'sass/_variables.scss';
+.v-application {
+    background-repeat: repeat !important;
+    background-color: #2771ad !important;
+    background-image: linear-gradient(to bottom, rgb(39, 113, 173, .24) 30%, rgb(0, 54, 94, 1) 30% 65%, rgb(39, 113, 173, 1) 65% 100%), url("data:image/svg+xml,%3Csvg width='84' height='48' viewBox='0 0 84 48' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M0 0h12v6H0V0zm28 8h12v6H28V8zm14-8h12v6H42V0zm14 0h12v6H56V0zm0 8h12v6H56V8zM42 8h12v6H42V8zm0 16h12v6H42v-6zm14-8h12v6H56v-6zm14 0h12v6H70v-6zm0-16h12v6H70V0zM28 32h12v6H28v-6zM14 16h12v6H14v-6zM0 24h12v6H0v-6zm0 8h12v6H0v-6zm14 0h12v6H14v-6zm14 8h12v6H28v-6zm-14 0h12v6H14v-6zm28 0h12v6H42v-6zm14-8h12v6H56v-6zm0-8h12v6H56v-6zm14 8h12v6H70v-6zm0 8h12v6H70v-6zM14 24h12v6H14v-6zm14-8h12v6H28v-6zM14 8h12v6H14V8zM0 8h12v6H0V8z' fill='%233d82b1' fill-opacity='0.24' fill-rule='evenodd'/%3E%3C/svg%3E") !important;  
+}
 .home-splash {
   background-image: url("/img/background-2.svg") !important;
   background-position:center;
   background-repeat: no-repeat;
   background-attachment: fixed;
+    min-height: 200px;
+  padding-top: 5rem;
+  h1 {
+    font-size:30px;
+  }
 }
-.home-splash {
-  min-height: 200px;
-  padding-top: 1.5rem;
-}
+
 .search-nearby-container{
   background: #BFDDF2 0% 0% no-repeat padding-box;
   border-radius: 16px;
@@ -262,25 +270,27 @@
 
     @media (min-width: 960px) {
       height: 900px;
-      background: linear-gradient(to bottom, #97CFF5 50%, #0075B2 50%);
+      //background-image: url(""), linear-gradient(to bottom, #00365E 50%, #0075B2 50%);
     }
   }
 
   .home-callout-container{
     margin-bottom: 80px;
-    height: 630px;
+    padding-bottom: 60px;
+
     background-color: white;
     box-shadow: 0px 3px 6px #00000029;
+    border-radius: 4px;
 
     @media (min-width: 960px) {
       margin-bottom: inherit;
-      height: 720px;
+
     }
   }
 
   .home-callout-top{
     background-color: #c4e4f9;
-    height: 70px;
+    height: 75px;
 
     h2{
       position: relative;
