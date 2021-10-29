c4fdd87bc10dee3a7a3a5b33b490a2303e118404
Alex Smith <alexsmith@rti.org>
2021-10-29 12:01:14 -0400

    data nav

--- src/components/DataNavigation.vue
+++ src/components/DataNavigation.vue
@@ -3,7 +3,7 @@
     <div class="data-navigation">
       <v-tabs
         hide-slider
-        background-color="grey lighten-3"
+        background-color="white"
         v-model="dataTabs"
         show-arrows
         class="ml-10"
@@ -21,18 +21,25 @@
 <style lang="scss" scoped>
 @import "~/sass/_variables";
 .data-navigation {
-  background-color: $lighter-gray;
+  background-color: $white;
+  box-shadow: 0 3px 1px -2px rgba(0, 0, 0, 0.2), 0 2px 2px 0 rgba(0, 0, 0, 0.14), 0 1px 5px 0 rgba(0, 0, 0, 0.12);
 }
-.v-tab--active {
-  background-color: $darker-green !important;
-  color: $white !important;
-  font-weight: bold !important;
-}
-.v-tabs-bar a {
+
+.v-tabs-bar
+  .v-tab--active {
+    background-color: $darker-green !important;
+    color: $white !important;
+    font-weight: bold !important;
+    a {
+      color:$white !important;
+    }
+  }
+ a {
   letter-spacing: 0 !important;
   font-family: $header-font-family !important;
   font-weight: normal;
-  font-size: 12px !important;
+  font-size: 14px !important;
+  color: #000 !important;
 }
 </style>
 <script>
