c34c3ae73bb9e518d54234fde602c104e8b9fd81
Alex Smith <alexsmith@rti.org>
2022-02-06 10:54:33 -0500

    merged gridsome version into dev

--- _plugins/build.rb
+++
@@ -1,6 +0,0 @@
-# Build assets for all branches
-
-if ENV['BASEURL'] != 'http://localhost:4000'
-    puts '[build.rb] Webpack is processing assets'
-    `(npm install; npm rebuild; npm run build) &> build.log`
-end
