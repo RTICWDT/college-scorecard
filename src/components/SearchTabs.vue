017f2590d55998b0e54c73ab0b0432288e658912
Brown, Patrick <patrickbrown@rti.org>
2023-02-14 17:05:30 -0800

    Fixing search tabs

--- src/components/SearchTabs.vue
+++ src/components/SearchTabs.vue
@@ -1,16 +1,19 @@
 <template>
-  <v-tabs slider-size="5" :value="selectedTab">
+  <v-tabs slider-size="5" :hide-slider="true">
     <div class="mb-0 px-5 d-flex align-self-center black--text light-grey">
       <strong>SEARCH:</strong>
     </div>
-    <v-tab to="/search/" :class="{ current: selectedTab == 0 }">
+    <v-tab to="/search/" class="school" :class="{ current: selected == 0 }">
       Schools
     </v-tab>
 
-    <v-tab to="/search/fos-landing" :class="{ current: selectedTab == 1 }">
+    <v-tab
+      to="/search/fos-landing"
+      class="fos"
+      :class="{ current: selected == 1 }"
+    >
       Fields of Study
     </v-tab>
-    <v-tabs-slider height="10" :color="sliderColor" />
   </v-tabs>
 </template>
 
@@ -19,9 +22,24 @@
   letter-spacing: normal;
   text-transform: none;
   color: #000000 !important;
+  position: relative;
+  &::after {
+    content: " ";
+    border: 3px none white;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+    position: absolute;
+    border-radius: 5px 5px 0 0;
+  }
 }
-.v-tabs-slider {
-  border-radius: 5px 5px 0 0;
+.v-tab.school.current::after {
+  border-color: #7bd88c;
+  border-style: solid;
+}
+.v-tab.fos.current::after {
+  border-color: #fdb022;
+  border-style: solid;
 }
 .current {
   font-weight: bold;
@@ -34,27 +52,6 @@ export default {
     selected: {
       type: Number,
       default: 0,
-    },
-  },
-  data() {
-    return {}
-  },
-  computed: {
-    sliderColor() {
-      switch (this.selected) {
-        case 0:
-          return "#7BD88C"
-          break
-        case 1:
-          return "#FDB022"
-          break
-        default:
-          this.sliderColor = "#FFFFFF88"
-          break
-      }
-    },
-    selectedTab() {
-      return this.selected || 0
     },
   },
 }
