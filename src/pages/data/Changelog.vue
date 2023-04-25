dcaebbff85076b15a988c439786f74983446354b
Alexander Smith <alexsmith@rti.org>
2023-04-25 10:23:08 -0400

    last minute fixes, update dates, new social image

--- src/pages/data/Changelog.vue
+++ src/pages/data/Changelog.vue
@@ -25,7 +25,7 @@
             <v-expansion-panels multiple v-model="panels">
               <v-expansion-panel>
                 <v-expansion-panel-header
-                  >April 25, 2022</v-expansion-panel-header
+                  >April 25, 2023</v-expansion-panel-header
                 >
                 <v-expansion-panel-content>
                   <p>
