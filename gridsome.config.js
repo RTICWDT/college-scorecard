6beaf5aae92c3b5a0a8e7b8a1bdd2c0a4b14a73b
Alexander Smith <alexsmith@rti.org>
2023-06-06 11:44:56 -0400

    back to top on search pagination and set to development for gridsome server

--- gridsome.config.js
+++ gridsome.config.js
@@ -6,10 +6,34 @@
 
 module.exports = {
   siteName: 'College Scorecard',
-  plugins: [],
+  plugins: [
+  ],
   pathPrefix: process.env.BASEURL || '/', //use for federalist
+  configureWebpack: {
+    
+  },
   chainWebpack (config) {
     config.mode('production')
+
+    /*const vueRule = config.module.rule('vue')
+    vueRule.uses.clear()
+
+    vueRule.test('/\.vue$/')
+      .use('vue-loader')
+      .loader('vue-loader')
+
+    config.module
+      .rule('babel-loader')
+        .test('/\.js$/')
+        .use('babel-loader')
+          .loader('babel-loader') 
+
+    config.module
+      .rule('css-loader')
+        .test('/\.css$/')
+        .use('css-loader')
+          .loader('vue-style-loader')
+          .loader('css-loader')         */           
   }, 
   siteDescription: "Find the college that’s the best fit for you! The U.S. Department of Education’s College Scorecard has the most reliable data on college costs, graduation, and post-college earnings."
 }
