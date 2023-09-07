62f344fa0958e685a905bfc1725eb07919195abd
Alexander Smith <alexsmith@rti.org>
2023-09-07 09:21:18 -0400

    fixed default search issue

--- src/components/SearchForm.vue
+++ src/components/SearchForm.vue
@@ -705,8 +705,10 @@ export default {
       // Send new param object, reset page
       this.$emit("search-query", { ...this.cleanInput, page: 0 })
     }, 1000)
-
-    //this.$emit("search-query", { ...this.cleanInput, page: 0 })
+    if (this.autoSubmit)
+    {
+      this.$emit("search-query", { ...this.cleanInput, page: 0 })
+    }
   },
   mounted() {
     this.$root.$on("search-form-reset", (e) => {
