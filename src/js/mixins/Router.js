0c96f8d6004934ad5f0aa8b41921959acbde6f30
Alexander Smith <alexsmith@rti.org>
2023-09-26 13:54:22 -0400

    fixed tab index reset on route change issue

--- src/js/mixins/Router.js
+++ src/js/mixins/Router.js
@@ -9,33 +9,6 @@ export default {
   },
   watch:{
     setQueryStringParameters(val,oldVal){
-    },
-    $route: function(to) {
-      this.$nextTick(function () {
-      // $nextTick = DOM updated
-    
-    // Change document title
-      document.title = to.meta.title;
-    
-        setTimeout(() => {
-
-          // Get component's "routeFocusTarget" ref. 
-          // If not existent, use router view container
-          let focusTarget = (this.$refs.routerView.$refs.componentFocusTarget !== undefined) 
-          ? this.$refs.routerView.$refs.componentFocusTarget
-          : this.$refs.routerView.$el;
-
-          // Make focustarget programmatically focussable
-          focusTarget.setAttribute('tabindex', '-1');
-
-          // Focus element
-          focusTarget.focus();
-
-          // Remove tabindex from focustarget. 
-          // Reason: https://axesslab.com/skip-links/#update-3-a-comment-from-gov-uk
-          focusTarget.removeAttribute('tabindex');
-        }, 0);
-      });
     }
   },
   methods:{
