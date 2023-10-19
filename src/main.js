ef1f5ad7e5e332e7989bb35d3bdbdab92287ef87
Alexander Smith <alexsmith@rti.org>
2023-10-19 09:14:42 -0400

    csp

--- src/main.js
+++ src/main.js
@@ -164,12 +164,12 @@ export default function (Vue, { router, head, isClient, appOptions }) {
   })   
 
   //Twitter and OpenGraph
-    /*
+    
   head.meta.push({
     'http-equiv':'Content-Security-Policy',
     content: "default-src 'self' https://www.gstatic.com https//*.typekit.net https://dap.digitalgov.gov https://static.hotjar.com https://www.google-analytics.com https://www.gstatic.com https://www.googletagmanager.com https://script.hotjar.com; img-src 'self' *.googletagmanager.com data: https://*.basemaps.cartocdn.com/ *.cloud.gov *.googleapis.com *.google-analytics.com; child-src 'none'; script-src-elem * 'unsafe-hashes' 'sha256-6egcWvmQP5hRCmW3iDq1X7sXo3/AY4zjQJNgqpm0rAg=' 'sha256-1e5RR2OpHhuX2h0Bat19DsNTmqbo4M3T1pqfeTXCHaA=' https://www.gstatic.com https://*.typekit.net/ https://fonts.googleapis.com 'self'; connect-src  https://api.data.gov https://www.google-analytics.com *.hotjar.com https://dap.digitalgov.gov https://stats.g.doubleclick.net *.hotjar.io 'self' ws:; font-src https://fonts.gstatic.com https://use.typekit.net 'self'; style-src 'unsafe-hashes' 'sha256-IQtfqzo2VPwYVfG45njd1UW4FZvVifJwM9V5cBNhx04=' 'sha256-yQomDylAVvpeRdlKgfqevQZtrtkp244GCzedeISNu5s=' 'sha256-vil6rwz9/3551namkTsVzfkGNkmRnO56lPD53U/pJbE=' 'sha256-cgjVz+andACCIsfeXoiI1/ChFQDlCC4y99sX5rrYseM=' 'sha256-TbrjG17MSiO8IKSlX/5IHYPweVR4+mHPUuUwZ7a5a2Y=' 'sha256-plI3P8SbP3ivQ+UUtO9JmesuN+PeYliWE+cQeiYVmiI=' 'sha256-uaE1UV3pZxXpavOfm/lrrxDNgEh6v+6g5hNCCp9X1bM=' 'sha256-lNOe1Kn+4LzGOr+ToMkETF58zM5uGP6/a0dq4hZYPYw=' 'sha256-xC8juOyOcGVG0BdxV2vy4LlzWOZfFlsV82FV45f1hTY=' 'sha256-hEgUMy467OqNfeKl/LTfR4/dPK2As3gne31r1nSBOEM=' 'sha256-jc9UupO4BKq2JkHD2Z57pylYRnr6PtjiI4FCyFavP+Q=' 'sha256-kwpt3lQZ21rs4cld7/uEm9qI5yAbjYzx+9FGm/XmwNU=' 'sha256-EjeKg7oj5a8SODh/FWP9+TlJK7I4FTcPMbwoz64kK2A=' 'sha256-uKAvZ8WKcIoeRdQwRKBztHzKuBMA3Z138XKPaddC+7w=' 'sha256-0EZqoz+oBhx7gF4nvY2bSqoGyy4zLjNF+SDQXGp/ZrY=' 'sha256-Q+nAeb6+lO7Hw/QTTOowxLK49HpHGB7ZfnCD6jX0qa4=' 'sha256-nr9cKw4ONTUDSB2DE0zh2RD8sdBaUN6T1YZ/cCE2H4M=' 'sha256-dkBZru08USynRApV+V+jfqq9MJtvAq+jnaGQA7L9XxU=' 'sha256-QdHgpwEEnPxZV6ByLX5uZCPOGKmc+SzQikh79cDYreg=' 'sha256-As+VZ5inwpJRs+3gKDhNonRoXR+TkIipyQpjbvYBHrU=' 'sha256-SaalYbFZ07L4qDYZ96HObgI4Re7qfskD24gbLnk5sMs=' 'sha256-jJoo4DNMwMVg4I4IDlg1ChLFJH7BV/MvNBPv6+rWzTc=' 'sha256-ANvVUHrEavvwj4x+omctWvh0ZmreLn18DtrkbfDSiLU=' 'sha256-47DEQpj8HBSa+/TImW+5JCeuQeRkm5NMpJWZG3hSuFU=' 'sha256-OwlOqbP3VnAzYedGO5K7BbLR2YOoHw96wRy+VxYn414=' https://fonts.googleapis.com https://www.gstatic.com https://p.typekit.net https://use.typekit.net 'self'; frame-src https://vars.hotjar.com https://www.google.com"
   })
-  
+  /*
 
   head.meta.push({
     'http-equiv':'Content-Security-Policy',
