dce9f355e055ede3e8a42ea98f01ba198a0d7a24
Alexander Smith <alexsmith@rti.org>
2024-06-10 15:27:43 -0400

    updated data documentation files

--- src/pages/data/Changelog.vue
+++ src/pages/data/Changelog.vue
@@ -25,21 +25,39 @@
             <v-expansion-panels multiple v-model="panels">
               <v-expansion-panel>
                 <v-expansion-panel-header
-                  >June 14, 2024</v-expansion-panel-header
+                  >June 13, 2024</v-expansion-panel-header
                 >
                 <v-expansion-panel-content>
                   <p>
-                    On June 14, 2024, the Department updated the College Scorecard website with more recent data ffor 
-                    metrics from the Internal Revenue Service (IRS). 
-                  </p>
-                  <p>
-                    Specific data updates in this release include:
+                    On June 13, 2024, 2024, the Department updated the College Scorecard website and datafiles and
                   </p>
                   <ul>
-                    <li>Added entries for earnings 5 years after completion and for earnings metrics 7, 9, and 11 years after entry</li>
-                    <li>Updated entries with new values due to the earnings data refresh and added entries for earnings 5 years after completion and for earnings metrics 7, 9, and 11 years after entry</li>
-                    <li>Added entries for earnings 5 years after completion</li>
-                    <li>Updated entries with new values due to the earnings data refresh and added entries for earnings 5 years after completion</li>                       
+                    <li>Updated annual data metrics with</li>
+                    <ul>
+                      <li>Refreshed institution-level earnings calculations for former students 6, 8, and 10 years 
+                      after starting school at the institution level.</li>
+                      <li>Refreshed institution and field of study earnings calculations for graduates 1 and 5 
+                        years after graduation (the 5-year calculation is new, previously the 4-year measurement 
+                        point was the furthest look back post-graduation).</li>
+                    </ul>
+                    <li>Restructured the data file to relabel previously published entry-cohort earnings calculations 
+                      released in 2022 and 2023 with the publication of a 
+                      <a
+                        @click="trackDownload('EarningsDataErrata.pdf')"
+                        :href="$url('/assets/EarningsDataErrata.pdf')"
+                        target="_blank"
+                        >data errata document
+                      </a>
+                       describing data quality 
+                      issues with those calculations.</li>
+                      <li>Updated data derived from Federal Student Aid sources to more recent values for the following data metrics:</li>
+                      <ul>
+                        <li>Currently Operating Status Flag</li>
+                        <li>Accrediting Agency Name and Code</li>
+                        <li>Title IV Approval Date</li>
+                        <li>Federal School Code</li>
+                        <li>Institutional control (Postsecondary Education Participants System version)</li>
+                      </ul>
                   </ul>
                 </v-expansion-panel-content>
               </v-expansion-panel>                             
