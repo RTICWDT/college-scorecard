fe015d4cbf1950ee05848b18892b27aade55c1fe
Alexander Smith <alexsmith@rti.org>
2024-09-13 16:51:53 -0400

    revert external link fix

--- src/js/mixins/AnalyticsEvents.js
+++ src/js/mixins/AnalyticsEvents.js
@@ -3,41 +3,26 @@ export default {
         trackAccordion(accordion) {
             this.GATrackEvent('[Data] Expand Accordion', accordion);
         },
-        trackOutboundLink(href) {
-
+        trackOutboundLink(evt) {
+            let href = '(unknown)';
+            // handle case for v-btn
+            if(evt.target.className.match(/v-btn__content/))
+            {
+                href = evt.target.parentNode.href;
+            }
+            else
+            {
+                href = evt.target.href;
+            }
             this.GATrackEvent('External Link', href);
 
         },
         transitionOutboundLink(event) {
-            let href = '(unknown)';
-            let host = '(unknown)';
-            // handle case for v-btn
-            if(event.target.className.match(/v-btn__content/))
-            {
-                href = event.target.parentNode.href;
-                host = event.target.parentNode.host;
+            if (confirm("You are leaving the College Scorecard. \n\nThe U.S. Department of Education does not necessarily endorse the views expressed or the data and facts presented on this external site. \n\nLinks are provided by the institutions and are only updated about once a year. As such, over the course of the year, some links may break or websites may be taken offline.")) {
+                this.trackOutboundLink(event);
             }
-            else
-            {
-                href = event.target.href;
-                host = event.target.host;
-            }
-
-            const rootDomain = s => {
-                const r =  /.*\.([^.]*[^0-9][^.]*\.[^.]*[^.0-9][^.]*$)/;
-                return s.replace(r, '$1');
-            };
-
-            if (!rootDomain(host).endsWith('.gov')) {
-                if (confirm("You are leaving the College Scorecard. \n\nThe U.S. Department of Education does not necessarily endorse the views expressed or the data and facts presented on this external site. \n\nLinks are provided by the institutions and are only updated about once a year. As such, over the course of the year, some links may break or websites may be taken offline.")) {
-                    this.trackOutboundLink(href);
-                }
-                else {
-                    event.preventDefault();
-                }
-            }
-            else  {
-                this.trackOutboundLink(href);
+            else {
+                event.preventDefault();
             }
             
           },        
