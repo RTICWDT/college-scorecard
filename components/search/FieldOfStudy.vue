ad0eaee019ef63038d117093a04a34f8365031ce
Marshall Hattersley <mwhatters@gmail.com>
2024-10-20 19:41:09 -0700

    experiemnting

--- components/search/FieldOfStudy.vue
+++ components/search/FieldOfStudy.vue
@@ -1,36 +1,5 @@
 <template>
-  <v-combobox
-    v-model="selectedItem"
-    :items="items"
-    item-title="title"
-    item-value="code"
-    return-object
-    :filter-keys="['raw.cip4Title', 'raw.title']"
-    filter-mode="union"
-
-    prepend-inner-icon="mdi-magnify"
-    color="primaryfos"
-    variant="outlined"
-    min-width="220"
-
-    :density="dense ? 'compact' : 'default'"
-    :label="dense ? '' : 'Type to search'"
-    :placeholder="dense ? 'Type to search' : 'Search for a field of study'"
-    aria-label="Field of Study Search"
-    autocomplete="off"
-    hide-no-data
-    hide-details
-    aria-required="true"
-    aria-haspopup="listbox"
-    required
-    :list-props="{ tag: 'ul' }"
-  >
-    <template #item="{ item, props }">
-      <v-list-item tag='li' role="option" v-bind="props">
-        <v-list-item-subtitle class="subtitle" v-html="item.raw.cip4Title" />
-      </v-list-item>
-    </template>
-  </v-combobox>
+  <CBox :items="items" v-model="selectedItem" />
 </template>
 
 <style scoped lang="scss">
