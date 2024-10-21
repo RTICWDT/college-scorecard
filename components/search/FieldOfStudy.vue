536caa5c68e41f2c56bd6505a4bbb3ffe076a884
Marshall Hattersley <mwhatters@gmail.com>
2024-10-21 13:46:24 -0700

    revert fos search

--- components/search/FieldOfStudy.vue
+++ components/search/FieldOfStudy.vue
@@ -1,5 +1,36 @@
 <template>
-  <CBox :items="options" v-model="selectedItem" />
+  <v-combobox
+    v-model="selectedItem"
+    :items="items"
+    item-title="title"
+    item-value="code"
+    return-object
+    :filter-keys="['raw.cip4Title', 'raw.title']"
+    filter-mode="union"
+
+    prepend-inner-icon="mdi-magnify"
+    color="primaryfos"
+    variant="outlined"
+    min-width="220"
+
+    :density="dense ? 'compact' : 'default'"
+    :label="dense ? '' : 'Type to search'"
+    :placeholder="dense ? 'Type to search' : 'Search for a field of study'"
+    aria-label="Field of Study Search"
+    autocomplete="off"
+    hide-no-data
+    hide-details
+    aria-required="true"
+    aria-haspopup="listbox"
+    required
+    :list-props="{ tag: 'ul' }"
+  >
+    <template #item="{ item, props }">
+      <v-list-item tag='li' role="option" v-bind="props">
+        <v-list-item-subtitle class="subtitle" v-html="item.raw.cip4Title" />
+      </v-list-item>
+    </template>
+  </v-combobox>
 </template>
 
 <style scoped lang="scss">
@@ -17,9 +48,7 @@ const props = defineProps({
 })
 
 const { site } = useSiteData()
-const options = ref(site.value.data.cip_6_digit.map((item) => {
-  return { id: item.code, text: item.title, subtitle: item.cip4Title }
-}))
+const items = site.value.data.cip_6_digit
 const selectedItem = ref()
 </script>
 
