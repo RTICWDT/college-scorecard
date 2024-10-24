a65b35bd0b45770da87846a69629d04f002de7e5
Marshall Hattersley <mwhatters@gmail.com>
2024-10-23 17:50:03 -0700

    fix tab not highlighting on load

--- components/layout/DataNavigation.vue
+++ components/layout/DataNavigation.vue
@@ -8,11 +8,11 @@
           hide-slider
           show-arrows
         >
-          <v-tab tabindex="0" class="data-tab navigation" @click="navigateTo('/data')" value="/data">Download the Data</v-tab>
-          <v-tab tabindex="0" class="data-tab navigation" @click="navigateTo('/data/data-documentation')" value="/data/data-documentation">Data Documentation</v-tab>
-          <v-tab tabindex="0" class="data-tab navigation" @click="navigateTo('/data/api-documentation')" value="/data/api-documentation">API Documentation</v-tab>
-          <v-tab tabindex="0" class="data-tab navigation" @click="navigateTo('/data/changelog')" value="/data/changelog">Change Log</v-tab>
-          <v-tab tabindex="0" class="data-tab navigation" @click="navigateTo('/data/glossary')" value="/data/glossary">Glossary</v-tab>
+          <v-tab tabindex="0" class="data-tab navigation" @click="navigateTo('/data')" value="data">Download the Data</v-tab>
+          <v-tab tabindex="0" class="data-tab navigation" @click="navigateTo('/data/data-documentation')" value="data-documentation">Data Documentation</v-tab>
+          <v-tab tabindex="0" class="data-tab navigation" @click="navigateTo('/data/api-documentation')" value="api-documentation">API Documentation</v-tab>
+          <v-tab tabindex="0" class="data-tab navigation" @click="navigateTo('/data/changelog')" value="changelog">Change Log</v-tab>
+          <v-tab tabindex="0" class="data-tab navigation" @click="navigateTo('/data/glossary')" value="glossary">Glossary</v-tab>
         </v-tabs>
       </v-container>
     </div>
@@ -21,16 +21,29 @@
 
 <script setup>
 const router = useRouter()
-const route = useRoute();
+const route = useRoute()
 const analytics = useAnalytics()
+
+const normalizePath = (path) => {
+  // Remove trailing slash if present
+  path = path.replace(/\/$/, '')
+  // Remove '/data/' prefix to get the base route
+  return path.replace(/^\/data\/?/, '').replace(/^\//, '') || 'data'
+}
 
 const navigateTo = (href) =>  {
   router.push(href)
   analytics.trackNavigation(href)
 }
 
-const tab = ref(route.path)
-watch(() => route.path, (newPath) => { tab.value = newPath })
+const tab = ref(normalizePath(route.path))
+watch(() => route.path, (newPath) => { 
+  tab.value = normalizePath(newPath)
+})
+
+onMounted(() => {
+  tab.value = normalizePath(route.path)
+})
 </script>
 
 <style scoped lang="scss">
