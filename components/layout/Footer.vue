7bf0f49cd4e853d9d5ea9940c113cc3a711b35df
Marshall Hattersley <mwhatters@gmail.com>
2024-10-29 11:33:55 -1000

    add footer links

--- components/layout/Footer.vue
+++ components/layout/Footer.vue
@@ -1,44 +1,65 @@
 <template>
   <div class="d-print-none">
-    <div
-      class="d-block pa-md-4 text-white bg-primary-blue"
-      app
-      absolute
-      style="z-index: 900;"
-    >
-      <v-container class="py-8">
+    <div class="d-block pa-md-4 text-white bg-primary-blue" style="z-index: 900;">
+      <v-container :fluid="isFluid" class="py-8">
         <v-row>
-          <v-col>
+          <v-col cols="6" md="9">
             <ul class="d-md-flex justify-space-between ma-0 pa-0">
-              <li>
-                <NuxtLink to="/" :class="{ 'nav-active': activeLink === '/' }">
+              <li class="mx-2 mb-4">
+                <NuxtLink to="/" class="font-weight-bold">
                   Home
                 </NuxtLink>
               </li>
-              <li>
-                <NuxtLink to="/data" :class="{ 'nav-active': activeLink === 'data' }">
-                  About the Data
-                </NuxtLink>
-              </li>
-              <li>
-                <NuxtLink to="/search" :class="{ 'nav-active': activeLink === 'search' }">
+              <li class="mx-2 mb-4">
+                <div class="font-weight-bold mb-1">
                   Search
+                </div>
+                <NuxtLink to="/search" class="d-block mb-1">
+                  Search Schools
+                </NuxtLink>
+                <NuxtLink to="search/fos-landing" class="d-block mb-1">
+                  Search Fields of Study
                 </NuxtLink>
               </li>
-              <li>
-                <NuxtLink
-                  :class="{ 'nav-active': activeLink === 'compare' }"
-                  to="/compare"
-                  aria-label="Navigate to compare page"
-                  @click="handleCompareLinkClick('/compare')"
-                >
+              <li class="mx-2 mb-4">
+                <div class="font-weight-bold mb-1">
                   Compare
+                </div>
+                <NuxtLink to="/compare/?toggle=institutions" class="d-block mb-1">
+                  Compare Schools
+                </NuxtLink>
+                <NuxtLink to="/compare/?toggle=fos" class="d-block mb-1">
+                  Compare Fields of Study
                 </NuxtLink>
               </li>
-              <li></li>
+              <li class="mx-2 mb-4">
+                <NuxtLink to="/resources" class="font-weight-bold">
+                  Resources
+                </NuxtLink>
+              </li>
+              <li class="mx-2 mb-4">
+                <div class="font-weight-bold mb-1">
+                  About the Data
+                </div>
+                <NuxtLink to="/data" class="d-block mb-1">
+                  Download the Data
+                </NuxtLink>
+                <NuxtLink to="/data/data-documentation" class="d-block mb-1">
+                  Data Documentation
+                </NuxtLink>
+                <NuxtLink to="/data/api-documentation" class="d-block mb-1">
+                  API Documentation
+                </NuxtLink>
+                <NuxtLink to="/data/change-log" class="d-block mb-1">
+                  Change Log
+                </NuxtLink>
+                <NuxtLink to="/data/glossary" class="d-block mb-1">
+                  Glossary
+                </NuxtLink>
+              </li>
             </ul>
           </v-col>
-          <v-col>
+          <v-col cols="6" md="3">
             <div class="float-right">
               <p class="mb-0">
                 <small>
@@ -109,8 +130,14 @@ a {
 <script setup>
 const route = useRoute()
 const activeLink = ref(null)
+const { breakpoints } = useVuetify()
 
 const version = ref('3.0.3') // TODO: implement a way to get the version
+
+const isFluid = computed(() => {
+  console.log(breakpoints.md.value)
+  return breakpoints.md.value
+})
 
 const setActiveLink = () => {
   const path = process.client ? window.location.pathname : ''
