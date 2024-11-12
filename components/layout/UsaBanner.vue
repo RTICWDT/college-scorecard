6bcb75e89c56b21f52731b185a5b2ffecd52d288
Marshall Hattersley <mwhatters@gmail.com>
2024-11-12 15:23:22 -0800

    update usabanner link color

--- components/layout/UsaBanner.vue
+++ components/layout/UsaBanner.vue
@@ -7,7 +7,7 @@
           <span> 
             An official website of the United States government
             <button tabindex="2" @click="toggleInfo" alt="Here's how you know">
-              <span style="color: #0071bb; text-decoration: underline;">
+              <span style="color: #0065A8; text-decoration: underline;">
                 <span class="pr-1">Here's how you know</span><i class="fas reveal-caret" :class="showInfo ? 'fa-chevron-up' : 'fa-chevron-down'"></i>
               </span>
             </button>
