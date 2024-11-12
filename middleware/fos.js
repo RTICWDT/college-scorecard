f18d0d5ea5c018871cec8fbc3c2425bc6b14eed0
Marshall Hattersley <mwhatters@gmail.com>
2024-11-12 11:39:00 -0800

    console logging for testing bugs

--- middleware/fos.js
+++ middleware/fos.js
@@ -1,13 +1,11 @@
 export default defineNuxtRouteMiddleware((to, from) => {
 
+  console.log(to, from)
   if (from.name === "search-fos-landing") {
     if (to.query.cip4 && to.query.cip4_degree) {
       return
     }
 
     return navigateTo('/search/fos-landing')
-  }
-
-
-  
+  }  
 })
